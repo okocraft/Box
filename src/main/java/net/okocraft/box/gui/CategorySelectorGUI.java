9bf684a482963d73fb5a12fd378e8d73f717c1c4
Siroshun09 <siro.shun8@gmail.com>
2019-08-21 05:15:17 +0900

    [Change] コード整形

--- src/main/java/net/okocraft/box/gui/CategorySelectorGUI.java
+++ src/main/java/net/okocraft/box/gui/CategorySelectorGUI.java
@@ -18,10 +18,10 @@
 
 package net.okocraft.box.gui;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
+import lombok.val;
+import net.okocraft.box.Box;
+import net.okocraft.box.util.GeneralConfig;
+import net.okocraft.box.util.MessageConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -39,10 +39,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
-import lombok.val;
-import net.okocraft.box.Box;
-import net.okocraft.box.util.GeneralConfig;
-import net.okocraft.box.util.MessageConfig;
+import java.util.List;
+import java.util.Optional;
 
 public class CategorySelectorGUI implements Listener {
 
@@ -56,7 +54,7 @@ public class CategorySelectorGUI implements Listener {
     public static final Inventory GUI = Bukkit.createInventory(null, 54, CONFIG.getCategorySelectionGuiName());
 
     private static List<Integer> flameSlots = List.of(
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 17, 18, 26, 27, 35, 36, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 17, 18, 26, 27, 35, 36, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53
     );
 
     private static CategorySelectorGUI categorySelector;
@@ -72,20 +70,21 @@ public class CategorySelectorGUI implements Listener {
         GUI.clear();
         ItemStack flame = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
         ItemMeta flameMeta = flame.getItemMeta();
-        if (flameMeta != null){
-        flameMeta.setDisplayName("§r");
-        flameMeta.getPersistentDataContainer().set(CATEGORY_SELECTOR_KEY, PersistentDataType.INTEGER, 1);
-        flame.setItemMeta(flameMeta);
-        flameSlots.forEach(slot -> GUI.setItem(slot, flame));
+        if (flameMeta != null) {
+            flameMeta.setDisplayName("§r");
+            flameMeta.getPersistentDataContainer().set(CATEGORY_SELECTOR_KEY, PersistentDataType.INTEGER, 1);
+            flame.setItemMeta(flameMeta);
+            flameSlots.forEach(slot -> GUI.setItem(slot, flame));
 
             GUI.addItem(CONFIG.getCategories().entrySet().stream().map(entry -> createItem(entry.getKey(), entry.getValue())).toArray(ItemStack[]::new));
-    }}
-    
+        }
+    }
+
     /**
      * categoryのアイコンなどの情報をsectionから引き出し、そのアイテムを作る。
-     * 
+     *
      * @param categoryName カテゴリ名
-     * @param section コンフィグ
+     * @param section      コンフィグ
      * @return アイテム
      */
     private static ItemStack createItem(String categoryName, ConfigurationSection section) {
@@ -139,7 +138,7 @@ public class CategorySelectorGUI implements Listener {
 
     /**
      * カテゴリ選択GUIへのクリックを検知して、適切なカテゴリGUIに遷移させる。
-     * 
+     *
      * @param event イベント
      */
     @EventHandler(priority = EventPriority.NORMAL)
@@ -151,11 +150,11 @@ public class CategorySelectorGUI implements Listener {
         val action = event.getAction();
         val inventory = event.getClickedInventory();
         val guiItem = GUI.getItem(0);
-        if (inventory == null || inventory.getItem(0) == null || guiItem == null ||!guiItem.isSimilar(inventory.getItem(0))) {
+        if (inventory == null || inventory.getItem(0) == null || guiItem == null || !guiItem.isSimilar(inventory.getItem(0))) {
             return;
         }
         event.setCancelled(true);
-        
+
         if (CONFIG.getDisabledWorlds().contains(event.getWhoClicked().getWorld().getName())) {
             player.sendMessage(MESSAGE_CONFIG.getDisabledWorld());
             return;
