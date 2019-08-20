52f1b2d717ac33a85f0347170649167859909551
Siroshun09 <siro.shun8@gmail.com>
2019-08-21 05:09:00 +0900

    [Change] コード整形

--- src/main/java/net/okocraft/box/command/BoxCommand.java
+++ src/main/java/net/okocraft/box/command/BoxCommand.java
@@ -18,14 +18,13 @@
 
 package net.okocraft.box.command;
 
-import java.util.List;
-
-import org.bukkit.command.CommandSender;
-
 import net.okocraft.box.Box;
 import net.okocraft.box.database.Database;
 import net.okocraft.box.util.GeneralConfig;
 import net.okocraft.box.util.MessageConfig;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
 
 public interface BoxCommand {
 
@@ -36,53 +35,53 @@ public interface BoxCommand {
 
     /**
      * 各コマンドの処理
-     * 
+     *
      * @param sender コマンドの実行者
-     * @param args 引数
+     * @param args   引数
      * @return コマンドが成功したらtrue
      */
     boolean runCommand(CommandSender sender, String[] args);
 
     /**
      * 各コマンドのタブ補完の処理
-     * 
+     *
      * @param sender コマンドの実行者
-     * @param args 引数
+     * @param args   引数
      * @return その時のタブ補完のリスト
      */
     List<String> runTabComplete(CommandSender sender, String[] args);
 
     /**
      * コマンドの名前を取得する。
-     * 
+     *
      * @return コマンドの名前
      */
     String getCommandName();
 
     /**
      * このコマンドの権限を取得する。
-     * 
+     *
      * @return 権限
      */
     String getPermissionNode();
 
     /**
      * 最低限必要な引数の長さを取得する。
-     * 
+     *
      * @return 最低限の引数の長さ
      */
     int getLeastArgLength();
 
     /**
      * コマンドの引数の内容を取得する。例: "/box autostoreList [page]"
-     * 
+     *
      * @return 引数の内容
      */
     String getUsage();
 
     /**
      * コマンドの説明を取得する。例: "アイテムの自動収納の設定をリストにして表示する。"
-     * 
+     *
      * @return コマンドの説明
      */
     String getDescription();
