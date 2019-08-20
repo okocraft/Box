a5296738db8c1b23c14bdd2d7641b30f780bce0f
Siroshun09 <siro.shun8@gmail.com>
2019-08-21 04:15:44 +0900

    [Update] コード整形

--- src/main/java/net/okocraft/box/util/OtherUtil.java
+++ src/main/java/net/okocraft/box/util/OtherUtil.java
@@ -18,27 +18,24 @@
 
 package net.okocraft.box.util;
 
-import java.util.Optional;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import org.bukkit.Bukkit;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginManager;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Optional;
 
 public class OtherUtil {
 
     /**
      * numberを解析してint型にして返す。numberのフォーマットがintではないときはdefを返す。
-     * 
+     *
+     * @param number 解析する文字列
+     * @param def    解析に失敗したときに返す数字
+     * @return int型の数字。
      * @author LazyGon
      * @since v1.1.0
-     * 
-     * @param number 解析する文字列
-     * @param def 解析に失敗したときに返す数字
-     * 
-     * @return int型の数字。
      */
     public static int parseIntOrDefault(@Nonnull String number, int def) {
         try {
@@ -50,14 +47,12 @@ public class OtherUtil {
 
     /**
      * numberを解析してlong型にして返す。numberのフォーマットがlongではないときはdefを返す。
-     * 
+     *
+     * @param number 解析する文字列
+     * @param def    解析に失敗したときに返す数字
+     * @return long型の数字。
      * @author LazyGon
      * @since v1.1.0
-     * 
-     * @param number 解析する文字列
-     * @param def 解析に失敗したときに返す数字
-     * 
-     * @return long型の数字。
      */
     public static long parseLongOrDefault(@Nonnull String number, long def) {
         try {
@@ -69,8 +64,8 @@ public class OtherUtil {
 
     /**
      * パーミッションを登録する。
-     * 
-     * @param permName 登録する権限の文字列
+     *
+     * @param permName       登録する権限の文字列
      * @param parentPermName {@code permName} の親権限の文字列
      */
     public static void registerPermission(String permName, @Nullable String parentPermName) {
@@ -88,7 +83,7 @@ public class OtherUtil {
 
     /**
      * パーミッションを登録する。
-     * 
+     *
      * @param permName 登録する権限の文字列
      */
     public static void registerPermission(String permName) {
