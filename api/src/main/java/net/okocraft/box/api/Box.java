6bedd3b1eebf76c61ecb617379b56e05051c4ceb
Siroshun09 <siro.shun8@gmail.com>
2020-07-17 23:01:51 +0900

    [Add] getUserManager, getItemManager

--- api/src/main/java/net/okocraft/box/api/Box.java
+++ api/src/main/java/net/okocraft/box/api/Box.java
@@ -1,7 +1,15 @@
 package net.okocraft.box.api;
 
+import net.okocraft.box.api.item.ItemManager;
+import net.okocraft.box.api.user.UserManager;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Box API クラス。
  */
 public interface Box {
+
+    @NotNull UserManager getUserManager();
+
+    @NotNull ItemManager getItemManager();
 }
