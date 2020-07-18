1d892372be8bbb6f66de99a53e174a129597e9f9
Siroshun09 <siro.shun8@gmail.com>
2020-07-18 23:37:18 +0900

    [Add] アイテムの所持数管理クラス

---
+++ plugin/src/main/java/net/okocraft/box/plugin/model/item/Stock.java
@@ -0,0 +1,50 @@
+package net.okocraft.box.plugin.model.item;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * アイテムの所持数を管理するクラス
+ */
+public class Stock {
+
+    private final int internalID;
+    private final Item item;
+    private int amount;
+    private boolean autoStore;
+
+    public Stock(int internalID, @NotNull Item item) {
+        this(internalID, item, 0, false);
+    }
+
+    public Stock(int internalID, @NotNull Item item, int amount, boolean autoStore) {
+        this.internalID = internalID;
+        this.item = item;
+        this.amount = amount;
+        this.autoStore = autoStore;
+    }
+
+    public int getInternalID() {
+        return internalID;
+    }
+
+    @NotNull
+    public Item getItem() {
+        return item;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public boolean isAutoStore() {
+        return autoStore;
+    }
+
+    public void setAutoStore(boolean autoStore) {
+        this.autoStore = autoStore;
+    }
+}
