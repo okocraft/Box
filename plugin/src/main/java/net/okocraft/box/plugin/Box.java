b5114a16e317f52d05585df98a6553d96ff3d013
Siroshun09 <siro.shun8@gmail.com>
2020-07-23 18:01:34 +0900

    [Add] onLoad, onEnable, onDisable

--- plugin/src/main/java/net/okocraft/box/plugin/Box.java
+++ plugin/src/main/java/net/okocraft/box/plugin/Box.java
@@ -3,4 +3,19 @@ package net.okocraft.box.plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Box extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
