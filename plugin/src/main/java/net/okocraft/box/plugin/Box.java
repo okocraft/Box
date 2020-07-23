cc77b63e0cb10e3fe49dee955be6e8bb2b7971c4
Siroshun09 <siro.shun8@gmail.com>
2020-07-24 03:45:01 +0900

    [Add] UserManager

--- plugin/src/main/java/net/okocraft/box/plugin/Box.java
+++ plugin/src/main/java/net/okocraft/box/plugin/Box.java
@@ -2,18 +2,21 @@ package net.okocraft.box.plugin;
 
 import net.okocraft.box.plugin.database.Storage;
 import net.okocraft.box.plugin.model.manager.ItemManager;
+import net.okocraft.box.plugin.model.manager.UserManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public final class Box extends JavaPlugin {
 
     private ItemManager itemManager;
+    private UserManager userManager;
 
     @Override
     public void onLoad() {
         // storage initialization
 
         itemManager = new ItemManager(this);
+        userManager = new UserManager(this);
     }
 
     @Override
@@ -34,5 +37,10 @@ public final class Box extends JavaPlugin {
     @NotNull
     public ItemManager getItemManager() {
         return itemManager;
+    }
+
+    @NotNull
+    public UserManager getUserManager() {
+        return userManager;
     }
 }
---
+++ plugin/src/main/java/net/okocraft/box/plugin/model/manager/UserManager.java
@@ -0,0 +1,56 @@
+package net.okocraft.box.plugin.model.manager;
+
+import net.okocraft.box.plugin.Box;
+import net.okocraft.box.plugin.model.User;
+import net.okocraft.box.result.UserCheckResult;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class UserManager {
+
+    private final Box plugin;
+
+    public UserManager(@NotNull Box plugin) {
+        this.plugin = plugin;
+    }
+
+    @NotNull
+    public User loadUser(@NotNull UUID uuid) {
+        try {
+            return plugin.getStorage().loadUser(uuid).join();
+        } catch (Throwable e) {
+            throw new IllegalStateException("Could not load user:" + uuid.toString(), e);
+        }
+    }
+
+    public void saveUser(@NotNull User user) {
+        try {
+            plugin.getStorage().saveUser(user).join();
+        } catch (Throwable e) {
+            throw new IllegalStateException("Could not save user:" + user.getName(), e);
+        }
+    }
+
+    @NotNull
+    public UserCheckResult checkUser(@NotNull UUID uuid, @NotNull String name) {
+        UserCheckResult checkResult;
+
+        try {
+            checkResult = plugin.getStorage().checkUser(uuid, name).join();
+        } catch (Throwable e) {
+            checkResult = UserCheckResult.EXCEPTION_OCCURS;
+        }
+
+        if (checkResult != UserCheckResult.NEW_PLAYER) {
+            return checkResult;
+        }
+
+        try {
+            plugin.getStorage().saveDefaultUserData(uuid).join();
+            return checkResult;
+        } catch (Throwable e) {
+            return UserCheckResult.EXCEPTION_OCCURS;
+        }
+    }
+}
