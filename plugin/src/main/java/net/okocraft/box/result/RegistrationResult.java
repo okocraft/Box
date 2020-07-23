6a6e641b0a47a324794e175a39e758b0a837d5db
Siroshun09 <siro.shun8@gmail.com>
2020-07-23 21:33:45 +0900

    [Add] ItemManager, RegistrationResult

---
+++ plugin/src/main/java/net/okocraft/box/result/RegistrationResult.java
@@ -0,0 +1,15 @@
+package net.okocraft.box.result;
+
+import net.okocraft.box.api.util.Result;
+
+public enum RegistrationResult implements Result {
+    SUCCESS,
+    ALREADY_REGISTERED,
+    EXCEPTION_OCCURS
+    ;
+
+    @Override
+    public boolean isSuccess() {
+        return this == SUCCESS;
+    }
+}
