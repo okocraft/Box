cb6479052dd73499e81e088caadcb8788accf6c1
Siroshun09 <siro.shun8@gmail.com>
2020-07-23 18:06:07 +0900

    [Add] getStorage

--- plugin/src/main/java/net/okocraft/box/plugin/Box.java
+++ plugin/src/main/java/net/okocraft/box/plugin/Box.java
@@ -1,6 +1,8 @@
 package net.okocraft.box.plugin;
 
+import net.okocraft.box.plugin.database.Storage;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class Box extends JavaPlugin {
 
@@ -17,5 +19,10 @@ public final class Box extends JavaPlugin {
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    @NotNull
+    public Storage getStorage() {
+        return null; // TODO
     }
 }
