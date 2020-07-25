340d8ef673d3a5ac65c4280246247618fa00ccf3
Siroshun09 <siro.shun8@gmail.com>
2020-07-25 17:31:49 +0900

    [Add] プレースホルダーの数字のセットメソッド

--- plugin/src/main/java/net/okocraft/box/plugin/locale/message/Message.java
+++ plugin/src/main/java/net/okocraft/box/plugin/locale/message/Message.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.NotNull;
 public enum Message {
 
     PREFIX("prefix", "&8[&6Box&8]&r "),
+    TEST("test", "Test message {} {}"),
     ;
 
     private final String path;
@@ -22,6 +23,25 @@ public enum Message {
 
     @NotNull
     public String getDefault() {
-        return def;
+        return setPlaceholderNumber();
+    }
+
+    @NotNull
+    private String setPlaceholderNumber() {
+        StringBuilder builder = new StringBuilder();
+        char[] b = def.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] == '{' && i + 1 < b.length && b[i + 1] == '}') {
+                builder.append('{').append(count).append('}');
+                count++;
+                i++;
+            } else {
+                builder.append(b[i]);
+            }
+        }
+
+        return builder.toString();
     }
 }
