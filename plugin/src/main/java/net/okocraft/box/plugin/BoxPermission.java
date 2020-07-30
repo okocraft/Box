63fdfa75bfc7e9954e50dd004d0d2a76ac3d9423
Siroshun09 <siro.shun8@gmail.com>
2020-07-30 17:34:38 +0900

    [Change] キャッシュではなく、クラス初期呼び出し時にすべて Permission を生成する

--- plugin/src/main/java/net/okocraft/box/plugin/BoxPermission.java
+++ plugin/src/main/java/net/okocraft/box/plugin/BoxPermission.java
@@ -1,16 +1,13 @@
 package net.okocraft.box.plugin;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import org.bukkit.permissions.Permissible;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum BoxPermission {
 
@@ -21,8 +18,19 @@ public enum BoxPermission {
     BOX_STICK_OPEN("box.stick.open", true),
     BOX_STICK_PLACE("box.stick.place", true),
     BOX_STICK_THROW("box.stick.throw", true);
-    private final static LoadingCache<BoxPermission, Permission> CACHE =
-            CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES).build(CacheLoader.from(BoxPermission::getPermission));
+
+    private final static Map<BoxPermission, Permission> PERMISSIONS;
+
+    static {
+        Map<BoxPermission, Permission> result = new HashMap<>();
+
+        for (BoxPermission perm : values()) {
+            PermissionDefault def = perm.isDefault() ? PermissionDefault.TRUE : PermissionDefault.OP;
+            result.put(perm, new Permission(perm.getNode(), def));
+        }
+
+        PERMISSIONS = Map.copyOf(result);
+    }
 
     private final String node;
     private final boolean def;
@@ -42,17 +50,12 @@ public enum BoxPermission {
     }
 
     public boolean has(@NotNull Permissible permissible) {
-        try {
-            return permissible.hasPermission(CACHE.get(this));
-        } catch (ExecutionException e) {
-            return permissible.hasPermission(getNode());
-        }
+        return permissible.hasPermission(PERMISSIONS.get(this));
     }
 
-    @Contract(" -> new")
     @NotNull
-    public Permission getPermission() {
-        PermissionDefault def = isDefault() ? PermissionDefault.TRUE : PermissionDefault.OP;
-        return new Permission(getNode(), def);
+    @Unmodifiable
+    public static Map<BoxPermission, Permission> getPermissions() {
+        return PERMISSIONS;
     }
 }
