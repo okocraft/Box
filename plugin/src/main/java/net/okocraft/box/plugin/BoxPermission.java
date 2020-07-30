ef6cf858197348ce4cbb9aba92224f952239b2f0
Siroshun09 <siro.shun8@gmail.com>
2020-07-30 17:24:55 +0900

    [Change] Permission を作成し、デフォルト値も定義できるようにする

--- plugin/src/main/java/net/okocraft/box/plugin/BoxPermission.java
+++ plugin/src/main/java/net/okocraft/box/plugin/BoxPermission.java
@@ -1,6 +1,16 @@
 package net.okocraft.box.plugin;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.bukkit.permissions.Permissible;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public enum BoxPermission {
 
@@ -10,8 +20,9 @@ public enum BoxPermission {
     BOX_STICK_CONSUME("box.stick.consume", true),
     BOX_STICK_OPEN("box.stick.open", true),
     BOX_STICK_PLACE("box.stick.place", true),
-    BOX_STICK_THROW("box.stick.throw", true)
-    ;
+    BOX_STICK_THROW("box.stick.throw", true);
+    private final static LoadingCache<BoxPermission, Permission> CACHE =
+            CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES).build(CacheLoader.from(BoxPermission::getPermission));
 
     private final String node;
     private final boolean def;
@@ -28,5 +39,20 @@ public enum BoxPermission {
 
     public boolean isDefault() {
         return def;
+    }
+
+    public boolean has(@NotNull Permissible permissible) {
+        try {
+            return permissible.hasPermission(CACHE.get(this));
+        } catch (ExecutionException e) {
+            return permissible.hasPermission(getNode());
+        }
+    }
+
+    @Contract(" -> new")
+    @NotNull
+    public Permission getPermission() {
+        PermissionDefault def = isDefault() ? PermissionDefault.TRUE : PermissionDefault.OP;
+        return new Permission(getNode(), def);
     }
 }
