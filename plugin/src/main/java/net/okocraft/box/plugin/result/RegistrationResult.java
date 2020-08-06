8fc48530c604933123ef621207b309c472ba1aeb
Siroshun09 <siro.shun8@gmail.com>
2020-08-06 21:10:26 +0900

    [Add] RegistrationResult

--- plugin/src/main/java/net/okocraft/box/plugin/result/RegistrationResult.java
+++ plugin/src/main/java/net/okocraft/box/plugin/result/RegistrationResult.java
@@ -2,12 +2,31 @@ package net.okocraft.box.plugin.result;
 
 import net.okocraft.box.api.util.Result;
 
+/**
+ * Box のデータベースへのアイテム登録を試行した結果を列挙するクラス。
+ */
 public enum RegistrationResult implements Result {
-    SUCCESS,
-    ALREADY_REGISTERED,
-    EXCEPTION_OCCURS
-    ;
 
+    /**
+     * 正常に登録完了
+     */
+    SUCCESS,
+
+    /**
+     * すでに登録済み
+     */
+    ALREADY_REGISTERED,
+
+    /**
+     * 例外が発生
+     */
+    EXCEPTION_OCCURS;
+
+    /**
+     * 結果が成功したことを示しているか。
+     *
+     * @return {@link RegistrationResult#SUCCESS} なら {@code true}, そうでなければ {@code false}
+     */
     @Override
     public boolean isSuccess() {
         return this == SUCCESS;
