f5f88102d25b73c5761630804f67fbdbc9d9c092
Siroshun09 <siro.shun8@gmail.com>
2020-08-06 21:12:21 +0900

    [Add] UserCheckResult

--- plugin/src/main/java/net/okocraft/box/plugin/result/UserCheckResult.java
+++ plugin/src/main/java/net/okocraft/box/plugin/result/UserCheckResult.java
@@ -2,13 +2,36 @@ package net.okocraft.box.plugin.result;
 
 import net.okocraft.box.api.util.Result;
 
+/**
+ * ユーザーをチェックした結果を列挙するクラス。
+ */
 public enum UserCheckResult implements Result {
-    NONE,
-    NEW_PLAYER,
-    RENAMED,
-    EXCEPTION_OCCURS
-    ;
 
+    /**
+     * 変更なし
+     */
+    NONE,
+
+    /**
+     * 新しいプレイヤー
+     */
+    NEW_PLAYER,
+
+    /**
+     * 改名後初ログイン
+     */
+    RENAMED,
+
+    /**
+     * 例外が発生
+     */
+    EXCEPTION_OCCURS;
+
+    /**
+     * 結果が成功したことを示しているか。
+     *
+     * @return {@link UserCheckResult#EXCEPTION_OCCURS} なら {@code false}, そうでなければ {@code true}
+     */
     @Override
     public boolean isSuccess() {
         return this != EXCEPTION_OCCURS;
