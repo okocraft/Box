8d15187d5d9c8ee10c2afbba36d6abec62f01677
Siroshun09 <siro.shun8@gmail.com>
2020-08-06 20:57:28 +0900

    [Add] ItemBuilder

--- plugin/src/main/java/net/okocraft/box/plugin/util/ItemBuilder.java
+++ plugin/src/main/java/net/okocraft/box/plugin/util/ItemBuilder.java
@@ -9,6 +9,9 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * {@link ItemStack} を構築するクラス。
+ */
 public final class ItemBuilder {
 
     private Material material;
@@ -17,34 +20,90 @@ public final class ItemBuilder {
     private boolean isGlowing;
     private int amount;
 
+    /**
+     * アイテムの {@link Material} をセットする。
+     * <p>
+     * このメソッドを実行しない、または {@code null} を渡した状態で {@link ItemBuilder#build()} を呼び出すと
+     * {@link NullPointerException} がスルーされる。
+     * <p>
+     * {@link Material} の種類によっては、他のメソッドで設定した値が正常に適用されない場合がある。
+     *
+     * @param material アイテムの {@link Material}
+     * @return このオブジェクトへの参照
+     */
     public ItemBuilder setMaterial(Material material) {
         this.material = material;
 
         return this;
     }
 
+    /**
+     * アイテムの表示名をセットする。
+     * <p>
+     * このメソッドを実行しない、または {@code null} 渡した状態で {@link ItemBuilder#build()} を呼び出しても
+     * そのまま {@link ItemStack} を構築が続行される。表示名はデフォルトのアイテム名となる (言語によって異なる)。
+     *
+     * @param displayName アイテムの表示名
+     * @return このオブジェクトへの参照
+     */
     public ItemBuilder setDisplayName(String displayName) {
         this.displayName = displayName;
 
         return this;
     }
 
+    /**
+     * アイテムの Lore をセットする。
+     * <p>
+     * このメソッドを実行しない、または {@code null} 渡した状態で {@link ItemBuilder#build()} を呼び出しても
+     * そのまま {@link ItemStack} を構築が続行される。Lore は設定されない。
+     *
+     * @param lore アイテムの Lore
+     * @return このオブジェクトへの参照
+     */
     public ItemBuilder setLore(List<String> lore) {
         this.lore = lore;
 
         return this;
     }
 
+    /**
+     * アイテムが光っているかをセットする。
+     * <p>
+     * このメソッドを実行しない、または {@code null} 渡した状態で {@link ItemBuilder#build()} を呼び出しても
+     * そのまま {@link ItemStack} を構築が続行される。アイテムは光らない。
+     * <p>
+     * 光らせる場合、内部的には {@link Enchantment#LURE} Lv.1 をセットし、エンチャントを非表示にする。
+     * {@link Material#FISHING_ROD} 以外はこのエンチャントは効果を発揮しない。
+     *
+     * @param glowing アイテムを光らせるなら {@code true}, そうでなければ {@code false}
+     * @return このオブジェクトへの参照
+     */
     public ItemBuilder setGlowing(boolean glowing) {
         isGlowing = glowing;
 
         return this;
     }
 
+    /**
+     * アイテムの個数をセットする。
+     * <p>
+     * このメソッドを実行しない、または 0 以下を渡した状態で {@link ItemBuilder#build()} を呼び出しても
+     * そのまま {@link ItemStack} を構築が続行される。アイテムの個数は 1 となる。
+     *
+     * @param amount アイテムの個数
+     * @return このオブジェクトへの参照
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * アイテムを構築する。
+     *
+     * @return 構築したアイテム
+     * @throws NullPointerException {@link ItemBuilder#setMaterial(Material)} が実行されていないか、 {@code null} が渡された状態である場合。
+     */
     public ItemStack build() {
         Objects.requireNonNull(material);
 
