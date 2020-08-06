4639787b4c344775a8ebe6dff85b236ac945ad86
Siroshun09 <siro.shun8@gmail.com>
2020-08-06 21:16:45 +0900

    [Add] Category

--- plugin/src/main/java/net/okocraft/box/plugin/category/Category.java
+++ plugin/src/main/java/net/okocraft/box/plugin/category/Category.java
@@ -7,6 +7,9 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * 一つのカテゴリを管理するクラス。
+ */
 public class Category {
 
     private final String name;
@@ -22,21 +25,46 @@ public class Category {
         this.items = items;
     }
 
+    /**
+     * カテゴリの名前を返す。
+     * <p>
+     * このメソッドで返される文字列は、{@link net.okocraft.box.plugin.config.CategoryConfig} でルートパスとして使用される。
+     *
+     * @return カテゴリの名前
+     */
     @NotNull
     public String getName() {
         return name;
     }
 
+    /**
+     * カテゴリの表示名を返す。
+     *
+     * @return カテゴリの表示名
+     */
     @NotNull
     public String getDisplayName() {
         return displayName;
     }
 
+    /**
+     * カテゴリのアイコンを返す。
+     *
+     * @return カテゴリのアイコン
+     */
     @NotNull
     public ItemStack getIcon() {
         return icon;
     }
 
+    /**
+     * カテゴリに含まれる {@link Item} を返す。
+     * <p>
+     * このメソッドで返されるリストは変更可能であるが、そのリストで要素を追加 / 削除 しても
+     * {@link net.okocraft.box.plugin.config.CategoryConfig} には保存されない。
+     *
+     * @return カテゴリに含まれる {@link Item}
+     */
     @NotNull
     public List<Item> getItems() {
         return items;
