2928bfad932b824db9c1a09c5e30aca0d940865c
Siroshun09 <siro.shun8@gmail.com>
2020-08-11 04:20:20 +0900

    [Change] /box の実行をプレイヤーのみに制限する

--- plugin/src/main/java/net/okocraft/box/plugin/command/commands/box/BoxCommand.java
+++ plugin/src/main/java/net/okocraft/box/plugin/command/commands/box/BoxCommand.java
@@ -3,6 +3,11 @@ package net.okocraft.box.plugin.command.commands.box;
 import net.okocraft.box.plugin.Box;
 import net.okocraft.box.plugin.BoxPermission;
 import net.okocraft.box.plugin.command.AbstractCommand;
+import net.okocraft.box.plugin.command.ArgumentList;
+import net.okocraft.box.plugin.locale.message.Message;
+import net.okocraft.box.plugin.result.CommandResult;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -18,5 +23,15 @@ public class BoxCommand extends AbstractCommand {
                 ),
                 BoxPermission.BOX_COMMAND
         );
+    }
+
+    @Override
+    public @NotNull CommandResult execute(@NotNull CommandSender sender, @NotNull ArgumentList args) {
+        if (sender instanceof Player) {
+            return super.execute(sender, args);
+        } else {
+            plugin.getLocaleLoader().format(Message.ERROR_ONLY_PLAYER, true).send(sender);
+            return CommandResult.NOT_PLAYER;
+        }
     }
 }
