f74478ed66296b549e581bef0e67b421c9d76313
Siroshun09 <siro.shun8@gmail.com>
2021-08-28 04:34:53 +0900

    change: remove BoxCustomItem#setDisplayName

--- api/src/main/java/net/okocraft/box/api/model/item/BoxCustomItem.java
+++ api/src/main/java/net/okocraft/box/api/model/item/BoxCustomItem.java
@@ -1,18 +1,7 @@
 package net.okocraft.box.api.model.item;
 
-import net.kyori.adventure.text.Component;
-import org.jetbrains.annotations.NotNull;
-
 /**
  * An interface for the added item.
  */
 public interface BoxCustomItem extends BoxItem {
-
-    /**
-     * Sets the display name
-     *
-     * @param component the display name
-     */
-    void setDisplayName(@NotNull Component component);
-
 }
