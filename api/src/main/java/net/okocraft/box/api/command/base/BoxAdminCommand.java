4e52fdf99a4c424654e47f03769067d10141a0b3
Siroshun09 <siro.shun8@gmail.com>
2021-08-28 03:42:24 +0900

    feat: add /box and /boxadmin command interface

---
+++ api/src/main/java/net/okocraft/box/api/command/base/BoxAdminCommand.java
@@ -0,0 +1,9 @@
+package net.okocraft.box.api.command.base;
+
+import net.okocraft.box.api.command.SubCommandHoldable;
+
+/**
+ * An interface of /boxadmin command.
+ */
+public interface BoxAdminCommand extends SubCommandHoldable {
+}
---
+++ api/src/main/java/net/okocraft/box/api/command/base/BoxCommand.java
@@ -0,0 +1,9 @@
+package net.okocraft.box.api.command.base;
+
+import net.okocraft.box.api.command.SubCommandHoldable;
+
+/**
+ * An interface of /box command.
+ */
+public interface BoxCommand extends SubCommandHoldable {
+}
