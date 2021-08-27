84ffbe49de7620064111333ced731d7d2a3e214e
Siroshun09 <siro.shun8@gmail.com>
2021-08-28 03:59:33 +0900

    feat: add BoxAPI and BoxProvider

---
+++ api/src/main/java/net/okocraft/box/api/BoxAPI.java
@@ -0,0 +1,12 @@
+package net.okocraft.box.api;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * An API of Box.
+ * <p>
+ * This interface extends the {@link Plugin},
+ * but it is intended for operations that require a plugin instance.
+ */
+public interface BoxAPI extends Plugin {
+}
