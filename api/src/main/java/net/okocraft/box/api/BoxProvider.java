d39012ea450a9825926c513471b70e4f010f98c2
Siroshun09 <siro.shun8@gmail.com>
2021-08-28 03:59:33 +0900

    feat: add BoxAPI and BoxProvider

---
+++ api/src/main/java/net/okocraft/box/api/BoxProvider.java
@@ -0,0 +1,39 @@
+package net.okocraft.box.api;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A class to provide {@link BoxAPI}.
+ */
+public final class BoxProvider {
+
+    private static BoxAPI API;
+
+    /**
+     * Gets a {@link BoxAPI}.
+     *
+     * @return a {@link BoxAPI}
+     * @throws IllegalStateException if {@link BoxAPI} is not loaded
+     */
+    public static @NotNull BoxAPI get() throws IllegalStateException {
+        if (API != null) {
+            return API;
+        } else {
+            throw new IllegalStateException("BoxAPI is not loaded.");
+        }
+    }
+
+    /**
+     * Sets a {@link BoxAPI}.
+     *
+     * @param api a {@link BoxAPI}
+     * @throws IllegalStateException if {@link BoxAPI} is already set
+     */
+    public static void set(@NotNull BoxAPI api) {
+        if (API == null) {
+            API = api;
+        } else {
+            throw new IllegalStateException("BoxAPI is already set.");
+        }
+    }
+}
