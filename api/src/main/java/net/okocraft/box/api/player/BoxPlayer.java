26ea9e1889c1c41379fbb287d7bf1f5de00e2a49
Siroshun09 <siro.shun8@gmail.com>
2021-08-28 09:35:13 +0900

    feat: add BoxPlayer and BoxPlayerMap

---
+++ api/src/main/java/net/okocraft/box/api/player/BoxPlayer.java
@@ -0,0 +1,33 @@
+package net.okocraft.box.api.player;
+
+import net.okocraft.box.api.model.stock.StockHolder;
+import net.okocraft.box.api.model.stock.UserStockHolder;
+import net.okocraft.box.api.model.user.BoxUser;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An interface of the online {@link BoxUser}.
+ */
+public interface BoxPlayer extends BoxUser {
+
+    /**
+     * Gets the {@link UserStockHolder} of this user.
+     *
+     * @return the {@link UserStockHolder} of this user
+     */
+    @NotNull UserStockHolder getUserStockHolder();
+
+    /**
+     * Gets the current {@link StockHolder} to deposit or withdraw.
+     *
+     * @return the current {@link StockHolder}
+     */
+    @NotNull StockHolder getCurrentStockHolder();
+
+    /**
+     * Sets the {@link StockHolder} to deposit or withdraw.
+     *
+     * @param stockHolder the {@link StockHolder}
+     */
+    void setCurrentStockHolder(@NotNull StockHolder stockHolder);
+}
