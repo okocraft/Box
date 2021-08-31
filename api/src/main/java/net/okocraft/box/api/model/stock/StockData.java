c2217eec8a31deab150d457eef9fd042a22a38e5
Siroshun09 <siro.shun8@gmail.com>
2021-08-31 11:51:17 +0900

    change(api): check arguments

--- api/src/main/java/net/okocraft/box/api/model/stock/StockData.java
+++ api/src/main/java/net/okocraft/box/api/model/stock/StockData.java
@@ -3,8 +3,15 @@ package net.okocraft.box.api.model.stock;
 import net.okocraft.box.api.model.item.BoxItem;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 /**
  * A record of the stock.
  */
 public record StockData(@NotNull BoxItem item, int amount) {
+
+    public StockData(@NotNull BoxItem item, int amount) {
+        this.item = Objects.requireNonNull(item);
+        this.amount = amount;
+    }
 }
