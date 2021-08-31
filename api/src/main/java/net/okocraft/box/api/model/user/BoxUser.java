cd4254b2a68822c86bd742d736a1d1097b6ffa1b
Siroshun09 <siro.shun8@gmail.com>
2021-08-31 10:25:57 +0900

    docs(api): improve BoxUser javadoc

--- api/src/main/java/net/okocraft/box/api/model/user/BoxUser.java
+++ api/src/main/java/net/okocraft/box/api/model/user/BoxUser.java
@@ -5,14 +5,14 @@ import org.jetbrains.annotations.NotNull;
 import java.util.UUID;
 
 /**
- * An interface of box user.
+ * An interface of a box user.
  */
 public interface BoxUser {
 
     /**
-     * Gets the {@link UUID} of the user.
+     * Gets the {@link UUID} of this user.
      *
-     * @return the {@link UUID} of the user.
+     * @return the {@link UUID} of this user.
      */
     @NotNull UUID getUUID();
 }
