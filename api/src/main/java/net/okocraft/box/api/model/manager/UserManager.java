120b771528e2e45f2501f5b70a0edd1b6aae13db
Siroshun09 <siro.shun8@gmail.com>
2021-08-31 12:02:18 +0900

    feat(api): add more methods

--- api/src/main/java/net/okocraft/box/api/model/manager/UserManager.java
+++ api/src/main/java/net/okocraft/box/api/model/manager/UserManager.java
@@ -3,6 +3,7 @@ package net.okocraft.box.api.model.manager;
 import net.okocraft.box.api.model.user.BoxUser;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -18,4 +19,20 @@ public interface UserManager {
      * @return the {@link CompletableFuture} to load the {@link BoxUser}
      */
     @NotNull CompletableFuture<@NotNull BoxUser> loadUser(@NotNull UUID uuid);
+
+    /**
+     * Saves the {@link BoxUser}.
+     *
+     * @param boxUser the user to save
+     * @return the {@link CompletableFuture} to save the {@link BoxUser}
+     */
+    @NotNull CompletableFuture<Void> saveUser(@NotNull BoxUser boxUser);
+
+    /**
+     * Searches for {@link BoxUser} with the specified name.
+     *
+     * @param name the name to search
+     * @return the {@link CompletableFuture} to search for {@link BoxUser}
+     */
+    @NotNull CompletableFuture<Optional<BoxUser>> search(@NotNull String name);
 }
