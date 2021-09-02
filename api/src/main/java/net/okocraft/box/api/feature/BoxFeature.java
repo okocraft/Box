cc5dc6343f123e209beb0f3c07149ffe44c225ff
Siroshun09 <siro.shun8@gmail.com>
2021-09-02 19:48:48 +0900

    feat(api): add BoxFeature#getListenerKey

--- api/src/main/java/net/okocraft/box/api/feature/BoxFeature.java
+++ api/src/main/java/net/okocraft/box/api/feature/BoxFeature.java
@@ -1,5 +1,6 @@
 package net.okocraft.box.api.feature;
 
+import com.github.siroshun09.event4j.handlerlist.Key;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -13,6 +14,13 @@ public interface BoxFeature {
      * @return the name of this feature
      */
     @NotNull String getName();
+
+    /**
+     * Gets the key of listeners.
+     *
+     * @return the key of listeners
+     */
+    @NotNull Key getListenerKey();
 
     /**
      * Enables this feature.
