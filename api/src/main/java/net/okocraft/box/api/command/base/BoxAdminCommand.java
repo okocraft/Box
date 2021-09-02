aec0bdd669bf3a41a14999b1aeb9e21744e5f703
Siroshun09 <siro.shun8@gmail.com>
2021-09-02 17:19:57 +0900

    feat(api): add Command interface

--- api/src/main/java/net/okocraft/box/api/command/base/BoxAdminCommand.java
+++ api/src/main/java/net/okocraft/box/api/command/base/BoxAdminCommand.java
@@ -1,9 +1,10 @@
 package net.okocraft.box.api.command.base;
 
+import net.okocraft.box.api.command.Command;
 import net.okocraft.box.api.command.SubCommandHoldable;
 
 /**
  * An interface of /boxadmin command.
  */
-public interface BoxAdminCommand extends SubCommandHoldable {
+public interface BoxAdminCommand extends Command, SubCommandHoldable {
 }
