831004d129dbc3716d32961b037cb393d8fe3fc2
Siroshun09 <siro.shun8@gmail.com>
2021-09-02 18:35:49 +0900

    feat(api): add #changeNoArgumentCommand

--- api/src/main/java/net/okocraft/box/api/command/base/BoxAdminCommand.java
+++ api/src/main/java/net/okocraft/box/api/command/base/BoxAdminCommand.java
@@ -2,9 +2,17 @@ package net.okocraft.box.api.command.base;
 
 import net.okocraft.box.api.command.Command;
 import net.okocraft.box.api.command.SubCommandHoldable;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * An interface of /boxadmin command.
  */
 public interface BoxAdminCommand extends Command, SubCommandHoldable {
+
+    /**
+     * Changes the command when there is no argument.
+     *
+     * @param command the command to change
+     */
+    void changeNoArgumentCommand(@NotNull Command command);
 }
