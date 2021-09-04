10e6187d87d44625ce6352fe940d40cd632f7ab8
Siroshun09 <siro.shun8@gmail.com>
2021-09-04 19:32:02 +0900

    feat(command): implement /boxadmin version

---
+++ command/src/main/java/net/okocraft/box/command/boxadmin/VersionCommand.java
@@ -0,0 +1,24 @@
+package net.okocraft.box.command.boxadmin;
+
+import net.okocraft.box.api.BoxProvider;
+import net.okocraft.box.api.command.AbstractCommand;
+import net.okocraft.box.command.message.CommandMessage;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public class VersionCommand extends AbstractCommand {
+
+    public VersionCommand() {
+        super("version", "box.command.version", Set.of("v", "ver"));
+    }
+
+    @Override
+    public void onCommand(@NotNull CommandSender sender, @NotNull String[] args) {
+        if (checkPermission(sender)) {
+            var version = BoxProvider.get().getPluginInstance().getDescription().getVersion();
+            sender.sendMessage(CommandMessage.VERSION_INFO.apply(version));
+        }
+    }
+}
