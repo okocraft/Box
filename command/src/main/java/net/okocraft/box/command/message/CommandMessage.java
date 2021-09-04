b0ca650d57261519a068711fb1cee156450294e2
Siroshun09 <siro.shun8@gmail.com>
2021-09-04 19:32:02 +0900

    feat(command): implement /boxadmin version

--- command/src/main/java/net/okocraft/box/command/message/CommandMessage.java
+++ command/src/main/java/net/okocraft/box/command/message/CommandMessage.java
@@ -1,6 +1,19 @@
 package net.okocraft.box.command.message;
 
+import net.okocraft.box.api.message.argument.SingleArgument;
+
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.Component.translatable;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
+
 public final class CommandMessage {
+
+    public static final SingleArgument<String> VERSION_INFO =
+            version -> translatable()
+                    .key("box.command.version.info")
+                    .args(text(version, AQUA))
+                    .color(GRAY)
+                    .build();
 
     private CommandMessage() {
         throw new UnsupportedOperationException();
