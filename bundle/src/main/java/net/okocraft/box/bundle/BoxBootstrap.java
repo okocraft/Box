dec0fe5f9ef5c9491bac587aebedb0f27d725e8b
Siroshun09 <siro.shun8@gmail.com>
2021-09-04 18:05:30 +0900

    feat(stick): make StickFeature bundled feature

--- bundle/src/main/java/net/okocraft/box/bundle/BoxBootstrap.java
+++ bundle/src/main/java/net/okocraft/box/bundle/BoxBootstrap.java
@@ -1,10 +1,17 @@
 package net.okocraft.box.bundle;
 
+import net.okocraft.box.api.feature.BoxFeature;
 import net.okocraft.box.core.BoxPlugin;
+import net.okocraft.box.stick.StickFeature;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public final class BoxBootstrap extends JavaPlugin {
+
+    private final List<BoxFeature> bundledFeatures =
+            List.of(new StickFeature());
 
     private BoxPlugin boxPlugin;
     private boolean isPaper;
@@ -47,11 +54,13 @@ public final class BoxBootstrap extends JavaPlugin {
             getServer().getPluginManager().disablePlugin(this);
         }
 
+        bundledFeatures.forEach(boxPlugin::register);
     }
 
     @Override
     public void onDisable() {
         if (isPaper && isLoaded) {
+            bundledFeatures.forEach(boxPlugin::unregister);
             boxPlugin.disable();
         }
     }
