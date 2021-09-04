21b4a01d87adf5ec4d4adb6938cfce657ae2c821
Siroshun09 <siro.shun8@gmail.com>
2021-09-04 18:05:30 +0900

    feat(stick): make StickFeature bundled feature

---
+++ stick/src/main/java/net/okocraft/box/stick/StickFeature.java
@@ -0,0 +1,32 @@
+package net.okocraft.box.stick;
+
+import net.okocraft.box.api.BoxProvider;
+import net.okocraft.box.api.feature.AbstractBoxFeature;
+import net.okocraft.box.stick.command.StickCommand;
+import net.okocraft.box.stick.item.BoxStickItem;
+import net.okocraft.box.stick.listener.StickListener;
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+
+public class StickFeature extends AbstractBoxFeature {
+
+    private final BoxStickItem boxStickItem = new BoxStickItem();
+    private final StickCommand stickCommand = new StickCommand(boxStickItem);
+    private final StickListener stickListener = new StickListener(boxStickItem);
+
+    public StickFeature() {
+        super("stick");
+    }
+
+    @Override
+    public void enable() {
+        BoxProvider.get().getBoxCommand().getSubCommandHolder().register(stickCommand);
+        Bukkit.getPluginManager().registerEvents(stickListener, BoxProvider.get().getPluginInstance());
+    }
+
+    @Override
+    public void disable() {
+        BoxProvider.get().getBoxCommand().getSubCommandHolder().unregister(stickCommand);
+        HandlerList.unregisterAll(stickListener);
+    }
+}
