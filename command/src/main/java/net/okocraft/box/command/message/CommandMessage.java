a7f895fd764d359ab0aa30ab3b9571538775510e
Siroshun09 <siro.shun8@gmail.com>
2021-09-04 19:46:23 +0900

    style(command): reformat codes

--- command/src/main/java/net/okocraft/box/command/message/CommandMessage.java
+++ command/src/main/java/net/okocraft/box/command/message/CommandMessage.java
@@ -4,16 +4,18 @@ import net.okocraft.box.api.message.argument.SingleArgument;
 
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.Component.translatable;
-import static net.kyori.adventure.text.format.NamedTextColor.*;
+import static net.kyori.adventure.text.format.NamedTextColor.AQUA;
+import static net.kyori.adventure.text.format.NamedTextColor.GRAY;
 
 public final class CommandMessage {
 
     public static final SingleArgument<String> VERSION_INFO =
-            version -> translatable()
-                    .key("box.command.version.info")
-                    .args(text(version, AQUA))
-                    .color(GRAY)
-                    .build();
+            version ->
+                    translatable()
+                            .key("box.command.version.info")
+                            .args(text(version, AQUA))
+                            .color(GRAY)
+                            .build();
 
     private CommandMessage() {
         throw new UnsupportedOperationException();
