acc92d2ffb25af6eebd4a491d3ec5cdf2f0c1919
Siroshun09 <siro.shun8@gmail.com>
2021-09-05 09:57:39 +0900

    feat(command): implement /boxadmin register

--- command/src/main/java/net/okocraft/box/command/message/BoxAdminMessage.java
+++ command/src/main/java/net/okocraft/box/command/message/BoxAdminMessage.java
@@ -3,13 +3,16 @@ package net.okocraft.box.command.message;
 import net.kyori.adventure.text.Component;
 import net.okocraft.box.api.message.argument.QuadArgument;
 import net.okocraft.box.api.message.argument.SingleArgument;
+import net.okocraft.box.api.model.item.BoxCustomItem;
 import net.okocraft.box.api.model.item.BoxItem;
+import org.bukkit.inventory.ItemStack;
 
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.Component.translatable;
 import static net.kyori.adventure.text.format.NamedTextColor.AQUA;
 import static net.kyori.adventure.text.format.NamedTextColor.GRAY;
 import static net.kyori.adventure.text.format.NamedTextColor.RED;
+import static net.kyori.adventure.text.format.NamedTextColor.WHITE;
 
 public final class BoxAdminMessage {
 
@@ -68,6 +71,35 @@ public final class BoxAdminMessage {
                             .build();
 
     public static final Component REGISTER_IS_AIR = translatable("box.command.boxadmin.register.is-air", RED);
+
+    public static final SingleArgument<ItemStack> REGISTER_ALREADY_REGISTERED =
+            item ->
+                    translatable()
+                            .key("box.command.boxadmin.register.already-registered")
+                            .args(item.displayName().hoverEvent(item))
+                            .color(RED)
+                            .build();
+
+    public static final SingleArgument<BoxCustomItem> REGISTER_SUCCESS =
+            item ->
+                    translatable()
+                            .key("box.command.boxadmin.register.success")
+                            .args(
+                                    item.getDisplayName().hoverEvent(item.getOriginal()),
+                                    text(item.getPlainName(), AQUA)
+                            )
+                            .color(GRAY)
+                            .build();
+
+    public static final Component REGISTER_TIP_RENAME = translatable("box.command.boxadmin.register.tip-rename", GRAY);
+
+    public static final SingleArgument<Throwable> REGISTER_FAILURE =
+            throwable ->
+                    translatable()
+                            .key("box.command.boxadmin.register.failure")
+                            .args(text(throwable.getMessage(), WHITE))
+                            .color(RED)
+                            .build();
 
     private BoxAdminMessage() {
         throw new UnsupportedOperationException();
