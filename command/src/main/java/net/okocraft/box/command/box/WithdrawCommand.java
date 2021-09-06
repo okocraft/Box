6d4731913ec850a18c6c9c4ad304295c40f36ad5
Siroshun09 <siro.shun8@gmail.com>
2021-09-06 17:40:25 +0900

    feat(command): implements /box withdraw

---
+++ command/src/main/java/net/okocraft/box/command/box/WithdrawCommand.java
@@ -0,0 +1,79 @@
+package net.okocraft.box.command.box;
+
+import net.okocraft.box.api.BoxProvider;
+import net.okocraft.box.api.command.AbstractCommand;
+import net.okocraft.box.api.message.GeneralMessage;
+import net.okocraft.box.api.transaction.InventoryTransaction;
+import net.okocraft.box.api.transaction.TransactionResultType;
+import net.okocraft.box.command.message.BoxMessage;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public class WithdrawCommand extends AbstractCommand {
+
+    public WithdrawCommand() {
+        super("withdraw", "box.command.withdraw", Set.of("w"));
+    }
+
+    @Override
+    public void onCommand(@NotNull CommandSender sender, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(GeneralMessage.ERROR_COMMAND_ONLY_PLAYER);
+            return;
+        }
+
+        if (args.length < 2) {
+            sender.sendMessage(GeneralMessage.ERROR_COMMAND_NOT_ENOUGH_ARGUMENT);
+            return;
+        }
+
+        var optionalBoxItem = BoxProvider.get().getItemManager().getBoxItem(args[1]);
+
+        if (optionalBoxItem.isEmpty()) {
+            sender.sendMessage(GeneralMessage.ERROR_COMMAND_ITEM_NOT_FOUND.apply(args[1]));
+            return;
+        }
+
+        var boxItem = optionalBoxItem.get();
+
+        var stockHolder = BoxProvider.get().getBoxPlayerMap().get(player).getCurrentStockHolder();
+
+        var currentStock = stockHolder.getAmount(boxItem);
+
+        if (currentStock < 1) {
+            sender.sendMessage(BoxMessage.WITHDRAW_NO_STOCK.apply(boxItem));
+            return;
+        }
+
+        int amount;
+
+        if (2 < args.length) {
+            try {
+                amount = Math.min(currentStock, Math.max(Integer.parseInt(args[2]), 1));
+            } catch (NumberFormatException e) {
+                player.sendMessage(GeneralMessage.ERROR_COMMAND_INVALID_NUMBER.apply(args[2]));
+                return;
+            }
+        } else {
+            amount = 1;
+        }
+
+        var result = InventoryTransaction.withdraw(player.getInventory(), boxItem, amount);
+        var resultType = result.getType();
+
+        if (resultType.isModified()) {
+            var current = stockHolder.decrease(result.getItem(), result.getAmount());
+
+            if (resultType == TransactionResultType.WITHDREW) {
+                player.sendMessage(BoxMessage.WITHDRAW_SUCCESS.apply(boxItem, amount, current));
+            } else {
+                player.sendMessage(BoxMessage.WITHDRAW_PARTIAL_SUCCESS.apply(boxItem, amount, current));
+            }
+        } else {
+            player.sendMessage(BoxMessage.WITHDRAW_INVENTORY_IS_FULL);
+        }
+    }
+}
--- command/src/main/java/net/okocraft/box/command/message/BoxMessage.java
+++ command/src/main/java/net/okocraft/box/command/message/BoxMessage.java
@@ -5,6 +5,7 @@ import net.okocraft.box.api.message.argument.SingleArgument;
 import net.okocraft.box.api.message.argument.TripleArgument;
 import net.okocraft.box.api.model.item.BoxItem;
 
+import static net.kyori.adventure.text.Component.newline;
 import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.Component.translatable;
 import static net.kyori.adventure.text.format.NamedTextColor.AQUA;
@@ -43,6 +44,35 @@ public final class BoxMessage {
 
     public static final Component DEPOSIT_NOT_FOUND =
             translatable("box.command.box.deposit.not-found", RED);
+
+    public static final TripleArgument<BoxItem, Integer, Integer> WITHDRAW_SUCCESS =
+            (item, amount, current) ->
+                    translatable()
+                            .key("box.command.box.withdraw.success")
+                            .args(
+                                    item.getDisplayName().color(AQUA).hoverEvent(item.getOriginal()),
+                                    text(amount, AQUA),
+                                    text(current, AQUA)
+                            )
+                            .color(GRAY)
+                            .build();
+
+    public static final TripleArgument<BoxItem, Integer, Integer> WITHDRAW_PARTIAL_SUCCESS =
+            (item, amount, current) ->
+                    translatable("command.withdraw.error.withdraw-stop", RED)
+                            .append(newline())
+                            .append(WITHDRAW_SUCCESS.apply(item, amount, current));
+
+    public static final SingleArgument<BoxItem> WITHDRAW_NO_STOCK =
+            item ->
+                    translatable()
+                            .key("box.command.box.withdraw.no-stock")
+                            .args(item.getDisplayName().color(AQUA).hoverEvent(item.getOriginal()))
+                            .color(RED)
+                            .build();
+
+    public static final Component WITHDRAW_INVENTORY_IS_FULL =
+            translatable("box.command.box.withdraw.inventory-full", RED);
 
     private BoxMessage() {
         throw new UnsupportedOperationException();
