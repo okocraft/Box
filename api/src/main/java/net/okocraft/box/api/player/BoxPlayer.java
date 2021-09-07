a447c042dd00ceaf63611d48b8ac24bda12b4a7f
Siroshun09 <siro.shun8@gmail.com>
2021-09-07 10:53:45 +0900

    feat(api): add BoxPlayer#getPlayer

--- api/src/main/java/net/okocraft/box/api/player/BoxPlayer.java
+++ api/src/main/java/net/okocraft/box/api/player/BoxPlayer.java
@@ -3,12 +3,20 @@ package net.okocraft.box.api.player;
 import net.okocraft.box.api.model.stock.StockHolder;
 import net.okocraft.box.api.model.stock.UserStockHolder;
 import net.okocraft.box.api.model.user.BoxUser;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * An interface of the online {@link BoxUser}.
  */
 public interface BoxPlayer extends BoxUser {
+
+    /**
+     * Gets the {@link Player} instance.
+     *
+     * @return the {@link Player}
+     */
+    @NotNull Player getPlayer();
 
     /**
      * Gets the {@link UserStockHolder} of this user.
