15a3f412034f481592e88ebf36370524d44a871b
Siroshun09 <siro.shun8@gmail.com>
2021-09-07 22:48:38 +0900

    feat(bundle): bundle autostore feature

--- bundle/src/main/java/net/okocraft/box/bundle/Bundled.java
+++ bundle/src/main/java/net/okocraft/box/bundle/Bundled.java
@@ -1,6 +1,7 @@
 package net.okocraft.box.bundle;
 
 import net.okocraft.box.api.feature.BoxFeature;
+import net.okocraft.box.autostore.AutoStoreFeature;
 import net.okocraft.box.command.CommandFeature;
 import net.okocraft.box.stick.StickFeature;
 
@@ -9,5 +10,5 @@ import java.util.List;
 final class Bundled {
 
     static final List<BoxFeature> FEATURES =
-            List.of(new CommandFeature(), new StickFeature());
+            List.of(new AutoStoreFeature(), new CommandFeature(), new StickFeature());
 }
