cc84d62c812d34d21af441c88a40cb1a3265263e
Siroshun09 <siro.shun8@gmail.com>
2021-09-09 17:24:11 +0900

    docs(api): add javadoc to CustomItemRegisterEvent

--- api/src/main/java/net/okocraft/box/api/event/item/CustomItemRegisterEvent.java
+++ api/src/main/java/net/okocraft/box/api/event/item/CustomItemRegisterEvent.java
@@ -4,14 +4,27 @@ import com.github.siroshun09.event4j.event.Event;
 import net.okocraft.box.api.model.item.BoxCustomItem;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * An event that is called when a new {@link BoxCustomItem} has been registered.
+ */
 public class CustomItemRegisterEvent extends Event {
 
     private final BoxCustomItem newItem;
 
+    /**
+     * The constructor of {@link CustomItemRegisterEvent}.
+     *
+     * @param newItem a registered {@link BoxCustomItem}
+     */
     public CustomItemRegisterEvent(@NotNull BoxCustomItem newItem) {
         this.newItem = newItem;
     }
 
+    /**
+     * Gets the registered {@link BoxCustomItem}.
+     *
+     * @return the registered {@link BoxCustomItem}
+     */
     public @NotNull BoxCustomItem getNewItem() {
         return newItem;
     }
