13ceb51fcab044a3550e3cfc6c81b113660f9419
Siroshun09 <siro.shun8@gmail.com>
2021-09-17 01:16:03 +0900

    refactor(gui): add MenuButton class

--- features/gui/src/main/java/net/okocraft/box/gui/internal/button/BackButton.java
+++ features/gui/src/main/java/net/okocraft/box/gui/internal/button/BackButton.java
@@ -1,27 +1,20 @@
 package net.okocraft.box.gui.internal.button;
 
-import net.okocraft.box.api.BoxProvider;
-import net.okocraft.box.gui.api.button.Button;
 import net.okocraft.box.gui.api.menu.Menu;
 import net.okocraft.box.gui.api.util.TranslationUtil;
-import net.okocraft.box.gui.internal.holder.BoxInventoryHolder;
 import net.okocraft.box.gui.internal.lang.Displays;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.concurrent.CompletableFuture;
+public class BackButton extends MenuButton {
 
-public class BackButton implements Button {
-
-    private final Menu menu;
     private final int slot;
 
     public BackButton(@NotNull Menu menu, int slot) {
-        this.menu = menu;
+        super(() -> menu);
         this.slot = slot;
     }
 
@@ -44,18 +37,5 @@ public class BackButton implements Button {
     @Override
     public int getSlot() {
         return slot;
-    }
-
-    @Override
-    public void onClick(@NotNull Player clicker, @NotNull ClickType clickType) {
-        var holder = new BoxInventoryHolder(menu);
-
-        holder.initializeMenu(clicker);
-        holder.applyContents();
-
-        CompletableFuture.runAsync(
-                () -> clicker.openInventory(holder.getInventory()),
-                BoxProvider.get().getExecutorProvider().getMainThread()
-        ).join();
     }
 }
