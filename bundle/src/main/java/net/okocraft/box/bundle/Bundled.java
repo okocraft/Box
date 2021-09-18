d5032563ee77b629e160f6a03ea058fbefdae349
Siroshun09 <siro.shun8@gmail.com>
2021-09-18 11:09:06 +0900

    feat(bundle): bundle craft feature

--- bundle/src/main/java/net/okocraft/box/bundle/Bundled.java
+++ bundle/src/main/java/net/okocraft/box/bundle/Bundled.java
@@ -4,6 +4,7 @@ import net.okocraft.box.api.feature.BoxFeature;
 import net.okocraft.box.feature.autostore.AutoStoreFeature;
 import net.okocraft.box.feature.category.CategoryFeature;
 import net.okocraft.box.feature.command.CommandFeature;
+import net.okocraft.box.feature.craft.CraftFeature;
 import net.okocraft.box.feature.stick.StickFeature;
 import net.okocraft.box.feature.gui.GuiFeature;
 
@@ -13,5 +14,5 @@ final class Bundled {
 
     static final List<BoxFeature> FEATURES =
             List.of(new CategoryFeature(), new AutoStoreFeature(), new CommandFeature(),
-                    new GuiFeature(), new StickFeature());
+                    new GuiFeature(), new CraftFeature(), new StickFeature());
 }
