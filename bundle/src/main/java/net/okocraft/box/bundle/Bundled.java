ae743a562d7c13fe7246d7ae55dfa734726db90e
Siroshun09 <siro.shun8@gmail.com>
2021-09-18 12:08:12 +0900

    change(bundle): change the order of loading features

--- bundle/src/main/java/net/okocraft/box/bundle/Bundled.java
+++ bundle/src/main/java/net/okocraft/box/bundle/Bundled.java
@@ -5,14 +5,15 @@ import net.okocraft.box.feature.autostore.AutoStoreFeature;
 import net.okocraft.box.feature.category.CategoryFeature;
 import net.okocraft.box.feature.command.CommandFeature;
 import net.okocraft.box.feature.craft.CraftFeature;
-import net.okocraft.box.feature.stick.StickFeature;
 import net.okocraft.box.feature.gui.GuiFeature;
+import net.okocraft.box.feature.stick.StickFeature;
 
 import java.util.List;
 
 final class Bundled {
 
     static final List<BoxFeature> FEATURES =
-            List.of(new CategoryFeature(), new AutoStoreFeature(), new CommandFeature(),
-                    new GuiFeature(), new CraftFeature(), new StickFeature());
+            List.of(new CategoryFeature(), new GuiFeature(), new CommandFeature(),
+                    new AutoStoreFeature(), new CraftFeature(), new StickFeature());
+
 }
