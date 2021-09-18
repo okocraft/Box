c97f585f01f3eac7190246a00c1aa4015970d0f2
Siroshun09 <siro.shun8@gmail.com>
2021-09-18 14:36:48 +0900

    feat(craft): make gui feature reloadable

--- features/gui/src/main/java/net/okocraft/box/feature/gui/GuiFeature.java
+++ features/gui/src/main/java/net/okocraft/box/feature/gui/GuiFeature.java
@@ -1,17 +1,24 @@
 package net.okocraft.box.feature.gui;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.okocraft.box.api.BoxProvider;
 import net.okocraft.box.api.feature.AbstractBoxFeature;
+import net.okocraft.box.api.feature.Reloadable;
 import net.okocraft.box.feature.gui.api.mode.ClickModeRegistry;
 import net.okocraft.box.feature.gui.internal.command.MenuOpenCommand;
 import net.okocraft.box.feature.gui.internal.holder.BoxInventoryHolder;
 import net.okocraft.box.feature.gui.internal.listener.InventoryListener;
 import net.okocraft.box.feature.gui.internal.mode.StorageMode;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
-public class GuiFeature extends AbstractBoxFeature {
+import java.util.concurrent.CompletableFuture;
+
+public class GuiFeature extends AbstractBoxFeature implements Reloadable {
 
     private final MenuOpenCommand command = new MenuOpenCommand();
     private final InventoryListener listener = new InventoryListener();
@@ -43,11 +50,37 @@ public class GuiFeature extends AbstractBoxFeature {
         boxCommand.changeNoArgumentCommand(null);
         boxCommand.getSubCommandHolder().unregister(command);
 
+        if (Bukkit.isPrimaryThread()) {
+            closeMenus();
+        } else {
+            CompletableFuture.runAsync(
+                    this::closeMenus,
+                    BoxProvider.get().getExecutorProvider().getMainThread()
+            ).join();
+        }
+
+        HandlerList.unregisterAll(listener);
+    }
+
+    private void closeMenus() {
         Bukkit.getOnlinePlayers()
                 .stream()
                 .filter(p -> p.getOpenInventory().getTopInventory().getHolder() instanceof BoxInventoryHolder)
                 .forEach(HumanEntity::closeInventory);
+    }
 
-        HandlerList.unregisterAll(listener);
+    @Override
+    public void reload(@NotNull CommandSender sender) {
+        disable();
+        enable();
+
+        try {
+            sender.sendMessage(Component.translatable("box.gui.command.reloaded", NamedTextColor.GRAY));
+        } catch (Exception ignored) {
+            // I don't know why it loops infinitely and throws an exception when the message send to the console.
+            // It's probably a bug of Paper or Adventure.
+            //
+            // IllegalStateException: Exceeded maximum depth of 512 while attempting to flatten components!
+        }
     }
 }
