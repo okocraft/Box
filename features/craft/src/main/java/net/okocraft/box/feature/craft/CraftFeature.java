1102241215c7e55f61f0796f59041e709c53de7f
Siroshun09 <siro.shun8@gmail.com>
2021-09-18 11:28:12 +0900

    feat(craft): implements /box craft <ITEM_NAME>

--- features/craft/src/main/java/net/okocraft/box/feature/craft/CraftFeature.java
+++ features/craft/src/main/java/net/okocraft/box/feature/craft/CraftFeature.java
@@ -1,13 +1,21 @@
 package net.okocraft.box.feature.craft;
 
+import net.okocraft.box.api.BoxProvider;
 import net.okocraft.box.api.feature.AbstractBoxFeature;
+import net.okocraft.box.api.model.item.BoxItem;
+import net.okocraft.box.feature.craft.command.CraftCommand;
 import net.okocraft.box.feature.craft.loader.RecipeLoader;
 import net.okocraft.box.feature.craft.mode.CraftMode;
+import net.okocraft.box.feature.craft.model2.RecipeHolder;
 import net.okocraft.box.feature.gui.api.mode.ClickModeRegistry;
+
+import java.util.Map;
 
 public class CraftFeature extends AbstractBoxFeature {
 
-    private final CraftMode craftMode = new CraftMode(RecipeLoader.load());
+    private final Map<BoxItem, RecipeHolder> recipeMap = RecipeLoader.load();
+    private final CraftMode craftMode = new CraftMode(recipeMap);
+    private final CraftCommand craftCommand = new CraftCommand(recipeMap);
 
     public CraftFeature() {
         super("craft");
@@ -16,10 +24,12 @@ public class CraftFeature extends AbstractBoxFeature {
     @Override
     public void enable() {
         ClickModeRegistry.register(craftMode);
+        BoxProvider.get().getBoxCommand().getSubCommandHolder().register(craftCommand);
     }
 
     @Override
     public void disable() {
         ClickModeRegistry.unregister(craftMode);
+        BoxProvider.get().getBoxCommand().getSubCommandHolder().unregister(craftCommand);
     }
 }
