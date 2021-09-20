7500f579d2339be31aa4dcac74e0d956a061e4fd
Siroshun09 <siro.shun8@gmail.com>
2021-09-20 18:44:50 +0900

    feat(core): make the debug log user-friendly

--- core/src/main/java/net/okocraft/box/core/listener/DebugListener.java
+++ core/src/main/java/net/okocraft/box/core/listener/DebugListener.java
@@ -1,19 +1,88 @@
 package net.okocraft.box.core.listener;
 
 import com.github.siroshun09.event4j.handlerlist.Key;
-import com.github.siroshun09.event4j.listener.Listener;
 import net.okocraft.box.api.BoxProvider;
 import net.okocraft.box.api.event.BoxEvent;
+import net.okocraft.box.api.event.player.PlayerLoadEvent;
+import net.okocraft.box.api.event.player.PlayerUnloadEvent;
+import net.okocraft.box.api.event.stock.StockDecreaseEvent;
+import net.okocraft.box.api.event.stock.StockIncreaseEvent;
+import net.okocraft.box.api.event.stock.StockSetEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class DebugListener {
 
-    private final Listener<BoxEvent> debugListener = event -> BoxProvider.get().getLogger().info("DEBUG: " + event);
+    private final Key listenerKey = Key.of("box:debug");
 
     public void register() {
-        BoxProvider.get().getEventBus().getHandlerList(BoxEvent.class).subscribe(Key.of("box:debug"), debugListener);
+        BoxProvider.get().getEventBus().getHandlerList(BoxEvent.class).subscribe(listenerKey, this::handleEvent);
     }
 
     public void unregister() {
-        BoxProvider.get().getEventBus().getHandlerList(BoxEvent.class).unsubscribe(debugListener);
+        BoxProvider.get().getEventBus().getHandlerList(BoxEvent.class).unsubscribeAll(listenerKey);
+    }
+
+    private void handleEvent(@NotNull BoxEvent event) {
+        if (event instanceof StockIncreaseEvent increaseEvent) {
+            printLog(
+                    increaseEvent.getEventName() + "{" +
+                            "stockholderName='" + increaseEvent.getStockHolder().getName() + "', " +
+                            "stockholderClass=" + increaseEvent.getStockHolder().getClass().getSimpleName() + ", " +
+                            "boxItem=" + increaseEvent.getItem() + ", " +
+                            "increments=" + increaseEvent.getIncrements() + ", " +
+                            "current=" + increaseEvent.getAmount() +
+                            "}"
+            );
+            return;
+        }
+
+        if (event instanceof StockDecreaseEvent decreaseEvent) {
+            printLog(
+                    decreaseEvent.getEventName() + "{" +
+                            "stockholderName='" + decreaseEvent.getStockHolder().getName() + "', " +
+                            "stockholderClass=" + decreaseEvent.getStockHolder().getClass().getSimpleName() + ", " +
+                            "boxItem=" + decreaseEvent.getItem() + ", " +
+                            "increments=" + decreaseEvent.getDecrements() + ", " +
+                            "current=" + decreaseEvent.getAmount() +
+                            "}"
+            );
+            return;
+        }
+
+        if (event instanceof StockSetEvent setEvent) {
+            printLog(
+                    setEvent.getEventName() + "{" +
+                            "stockholderName='" + setEvent.getStockHolder().getName() + "', " +
+                            "stockholderClass=" + setEvent.getStockHolder().getClass().getSimpleName() + ", " +
+                            "boxItem=" + setEvent.getItem() + ", " +
+                            "current=" + setEvent.getAmount() +
+                            "}"
+            );
+            return;
+        }
+
+        if (event instanceof PlayerLoadEvent loadEvent) {
+            printLog(loadEvent.getEventName() + "{" +
+                    "playerUuid=" + loadEvent.getBoxPlayer().getUUID() + ", " +
+                    "playerName='" + loadEvent.getBoxPlayer().getPlayer().getName() + "'" +
+                    "}"
+            );
+            return;
+        }
+
+        if (event instanceof PlayerUnloadEvent unloadEvent) {
+            printLog(unloadEvent.getEventName() + "{" +
+                    "playerUuid=" + unloadEvent.getBoxPlayer().getUUID() + ", " +
+                    "playerName='" + unloadEvent.getBoxPlayer().getPlayer().getName() + "'" +
+                    "}"
+            );
+            return;
+        }
+
+        printLog(event.toString());
+    }
+
+    private void printLog(@NotNull String log) {
+        BoxProvider.get().getLogger().info("DEBUG: " + log);
     }
 }
