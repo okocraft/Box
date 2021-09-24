6d8e9ee2cd259601807346cc29b64bf621f172f4
Siroshun09 <siro.shun8@gmail.com>
2021-09-24 19:52:55 +0900

    docs(api): add javadoc to AutoSaveStartEvent

--- api/src/main/java/net/okocraft/box/api/event/general/AutoSaveStartEvent.java
+++ api/src/main/java/net/okocraft/box/api/event/general/AutoSaveStartEvent.java
@@ -2,5 +2,8 @@ package net.okocraft.box.api.event.general;
 
 import net.okocraft.box.api.event.BoxEvent;
 
+/**
+ * An event called when the auto save task started.
+ */
 public class AutoSaveStartEvent extends BoxEvent {
 }
