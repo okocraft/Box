2d9fab89d091e6f51d61cedc6428db531e4da9fb
Siroshun09 <siro.shun8@gmail.com>
2021-10-12 15:48:31 +0900

    change(api): deprecates StockSaveEvent

--- api/src/main/java/net/okocraft/box/api/event/stock/StockSaveEvent.java
+++ api/src/main/java/net/okocraft/box/api/event/stock/StockSaveEvent.java
@@ -1,11 +1,14 @@
 package net.okocraft.box.api.event.stock;
 
 import net.okocraft.box.api.model.stock.StockHolder;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * A {@link StockEvent} called when the stockholder has been saved.
  */
+@Deprecated(forRemoval = true)
+@ApiStatus.ScheduledForRemoval(inVersion = "4.2.0")
 public class StockSaveEvent extends StockEvent{
 
     /**
