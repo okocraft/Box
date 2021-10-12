e3166351990f48989c808e03a8e00de797448224
Siroshun09 <siro.shun8@gmail.com>
2021-10-13 01:15:34 +0900

    improve(core): conditional branches

--- core/src/main/java/net/okocraft/box/core/listener/DebugListener.java
+++ core/src/main/java/net/okocraft/box/core/listener/DebugListener.java
@@ -3,13 +3,11 @@ package net.okocraft.box.core.listener;
 import com.github.siroshun09.event4j.handlerlist.Key;
 import net.okocraft.box.api.BoxProvider;
 import net.okocraft.box.api.event.BoxEvent;
-import net.okocraft.box.api.event.player.PlayerLoadEvent;
-import net.okocraft.box.api.event.player.PlayerUnloadEvent;
+import net.okocraft.box.api.event.player.PlayerEvent;
 import net.okocraft.box.api.event.stock.StockDecreaseEvent;
 import net.okocraft.box.api.event.stock.StockIncreaseEvent;
 import net.okocraft.box.api.event.stock.StockSetEvent;
-import net.okocraft.box.api.event.stockholder.StockHolderLoadEvent;
-import net.okocraft.box.api.event.stockholder.StockHolderSaveEvent;
+import net.okocraft.box.api.event.stockholder.StockHolderEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class DebugListener {
@@ -25,77 +23,58 @@ public class DebugListener {
     }
 
     private void handleEvent(@NotNull BoxEvent event) {
-        if (event instanceof StockIncreaseEvent increaseEvent) {
+        if (event instanceof StockHolderEvent stockHolderEvent) {
+            if (event instanceof StockIncreaseEvent increaseEvent) {
+                printLog(
+                        increaseEvent.getEventName() + "{" +
+                                "stockholderName='" + increaseEvent.getStockHolder().getName() + "', " +
+                                "stockholderClass=" + increaseEvent.getStockHolder().getClass().getSimpleName() + ", " +
+                                "boxItem=" + increaseEvent.getItem() + ", " +
+                                "increments=" + increaseEvent.getIncrements() + ", " +
+                                "current=" + increaseEvent.getAmount() +
+                                "}"
+                );
+                return;
+            }
+
+            if (event instanceof StockDecreaseEvent decreaseEvent) {
+                printLog(
+                        decreaseEvent.getEventName() + "{" +
+                                "stockholderName='" + decreaseEvent.getStockHolder().getName() + "', " +
+                                "stockholderClass=" + decreaseEvent.getStockHolder().getClass().getSimpleName() + ", " +
+                                "boxItem=" + decreaseEvent.getItem() + ", " +
+                                "decrements=" + decreaseEvent.getDecrements() + ", " +
+                                "current=" + decreaseEvent.getAmount() +
+                                "}"
+                );
+                return;
+            }
+
+            if (event instanceof StockSetEvent setEvent) {
+                printLog(
+                        setEvent.getEventName() + "{" +
+                                "stockholderName='" + setEvent.getStockHolder().getName() + "', " +
+                                "stockholderClass=" + setEvent.getStockHolder().getClass().getSimpleName() + ", " +
+                                "boxItem=" + setEvent.getItem() + ", " +
+                                "current=" + setEvent.getAmount() +
+                                "}"
+                );
+                return;
+            }
+
             printLog(
-                    increaseEvent.getEventName() + "{" +
-                            "stockholderName='" + increaseEvent.getStockHolder().getName() + "', " +
-                            "stockholderClass=" + increaseEvent.getStockHolder().getClass().getSimpleName() + ", " +
-                            "boxItem=" + increaseEvent.getItem() + ", " +
-                            "increments=" + increaseEvent.getIncrements() + ", " +
-                            "current=" + increaseEvent.getAmount() +
+                    stockHolderEvent.getEventName() + "{" +
+                            "stockholderName='" + stockHolderEvent.getStockHolder().getName() + "', " +
+                            "stockholderClass=" + stockHolderEvent.getStockHolder().getClass().getSimpleName() +
                             "}"
             );
             return;
         }
 
-        if (event instanceof StockDecreaseEvent decreaseEvent) {
-            printLog(
-                    decreaseEvent.getEventName() + "{" +
-                            "stockholderName='" + decreaseEvent.getStockHolder().getName() + "', " +
-                            "stockholderClass=" + decreaseEvent.getStockHolder().getClass().getSimpleName() + ", " +
-                            "boxItem=" + decreaseEvent.getItem() + ", " +
-                            "decrements=" + decreaseEvent.getDecrements() + ", " +
-                            "current=" + decreaseEvent.getAmount() +
-                            "}"
-            );
-            return;
-        }
-
-        if (event instanceof StockSetEvent setEvent) {
-            printLog(
-                    setEvent.getEventName() + "{" +
-                            "stockholderName='" + setEvent.getStockHolder().getName() + "', " +
-                            "stockholderClass=" + setEvent.getStockHolder().getClass().getSimpleName() + ", " +
-                            "boxItem=" + setEvent.getItem() + ", " +
-                            "current=" + setEvent.getAmount() +
-                            "}"
-            );
-            return;
-        }
-
-        if (event instanceof StockHolderLoadEvent loadEvent) {
-            printLog(
-                    loadEvent.getEventName() + "{" +
-                            "stockholderName='" + loadEvent.getStockHolder().getName() + "', " +
-                            "stockholderClass=" + loadEvent.getStockHolder().getClass().getSimpleName() +
-                            "}"
-            );
-            return;
-        }
-
-        if (event instanceof StockHolderSaveEvent saveEvent) {
-            printLog(
-                    saveEvent.getEventName() + "{" +
-                            "stockholderName='" + saveEvent.getStockHolder().getName() + "', " +
-                            "stockholderClass=" + saveEvent.getStockHolder().getClass().getSimpleName() +
-                            "}"
-            );
-            return;
-        }
-
-        if (event instanceof PlayerLoadEvent loadEvent) {
-            printLog(loadEvent.getEventName() + "{" +
-                    "playerUuid=" + loadEvent.getBoxPlayer().getUUID() + ", " +
-                    "playerName='" + loadEvent.getBoxPlayer().getName() + "'" +
-                    "}"
-            );
-            return;
-        }
-
-        if (event instanceof PlayerUnloadEvent unloadEvent) {
-            printLog(unloadEvent.getEventName() + "{" +
-                    "playerUuid=" + unloadEvent.getBoxPlayer().getUUID() + ", " +
-                    "playerName='" + unloadEvent.getBoxPlayer().getName() + "'" +
+        if (event instanceof PlayerEvent playerEvent) {
+            printLog(playerEvent.getEventName() + "{" +
+                    "playerUuid=" + playerEvent.getBoxPlayer().getUUID() + ", " +
+                    "playerName='" + playerEvent.getBoxPlayer().getName() + "'" +
                     "}"
             );
             return;
