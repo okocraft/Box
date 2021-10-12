518109e8361da0cecfbc5b49897e819ef4276d13
Siroshun09 <siro.shun8@gmail.com>
2021-10-12 15:43:39 +0900

    feat(api): add loading / saving stockholder event

---
+++ api/src/main/java/net/okocraft/box/api/event/stockholder/StockHolderSaveEvent.java
@@ -0,0 +1,19 @@
+package net.okocraft.box.api.event.stockholder;
+
+import net.okocraft.box.api.model.stock.StockHolder;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A {@link StockHolderEvent} called when the stockholder is saved.
+ */
+public class StockHolderSaveEvent extends StockHolderEvent {
+
+    /**
+     * The constructor of {@link StockHolderSaveEvent}.
+     *
+     * @param stockHolder the saved stockholder
+     */
+    public StockHolderSaveEvent(@NotNull StockHolder stockHolder) {
+        super(stockHolder);
+    }
+}
