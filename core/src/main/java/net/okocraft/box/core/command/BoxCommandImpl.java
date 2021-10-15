eda28b68657723337e6fc49849477e327a429f4c
Siroshun09 <siro.shun8@gmail.com>
2021-10-15 19:13:40 +0900

    change(command): prevents execution when the player data hasn't been loaded

--- core/src/main/java/net/okocraft/box/core/command/BoxCommandImpl.java
+++ core/src/main/java/net/okocraft/box/core/command/BoxCommandImpl.java
@@ -1,7 +1,11 @@
 package net.okocraft.box.core.command;
 
 import net.kyori.adventure.text.Component;
+import net.okocraft.box.api.BoxProvider;
 import net.okocraft.box.api.command.base.BoxCommand;
+import net.okocraft.box.api.message.GeneralMessage;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
@@ -36,5 +40,17 @@ public class BoxCommandImpl extends BaseCommand implements BoxCommand {
                 .append(text(" - ", DARK_GRAY))
                 .append(translatable("box.command.box.help.description", GRAY))
                 .build();
+    }
+
+    @Override
+    public void onCommand(@NotNull CommandSender sender, @NotNull String[] args) {
+        if (sender instanceof Player player) {
+            if (!BoxProvider.get().getBoxPlayerMap().isLoaded(player)) {
+                sender.sendMessage(GeneralMessage.ERROR_PLAYER_NOT_LOADED);
+                return;
+            }
+        }
+
+        super.onCommand(sender, args);
     }
 }
