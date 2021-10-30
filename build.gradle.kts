bc2c3370c3bcfd127d592ca452de452e7ff14e63
Siroshun09 <siro.shun8@gmail.com>
2021-10-30 20:33:22 +0900

    build: set the javadoc title [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,5 +11,7 @@ tasks {
             "feature/category/model/Category.java",
             "feature/gui/api/**"
         ).forEach { include("net/okocraft/box/$it") }
+
+        (options as StandardJavadocDocletOptions).docTitle("Box-$version")
     }
 }
