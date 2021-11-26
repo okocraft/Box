88c6faed3118230bcc70a962abe177bbd3766955
Siroshun09 <siro.shun8@gmail.com>
2021-11-26 19:56:26 +0900

    build: update event4j to 2.2.0

--- build-logic/src/main/kotlin/box.dependencies.gradle.kts
+++ build-logic/src/main/kotlin/box.dependencies.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation("com.github.siroshun09.configapi:configapi-yaml:4.5.0")
-    implementation("com.github.siroshun09.event4j:event4j:2.1.1")
+    implementation("com.github.siroshun09.event4j:event4j:2.2.0")
 
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:23.0.0")
