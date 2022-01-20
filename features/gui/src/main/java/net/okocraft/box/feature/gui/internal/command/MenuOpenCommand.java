1e3328fce68a34833f33ab42768f4ecf28bd64dd
Siroshun09 <siro.shun8@gmail.com>
2022-01-20 23:29:00 +0900

    feat(gui): calls added events

--- features/gui/src/main/java/net/okocraft/box/feature/gui/internal/command/MenuOpenCommand.java
+++ features/gui/src/main/java/net/okocraft/box/feature/gui/internal/command/MenuOpenCommand.java
@@ -24,10 +24,16 @@ import static net.kyori.adventure.text.Component.translatable;
 import static net.kyori.adventure.text.format.NamedTextColor.AQUA;
 import static net.kyori.adventure.text.format.NamedTextColor.DARK_GRAY;
 import static net.kyori.adventure.text.format.NamedTextColor.GRAY;
+import static net.kyori.adventure.text.format.NamedTextColor.RED;
 
 public class MenuOpenCommand extends AbstractCommand {
 
     private static final String OTHER_PLAYERS_GUI_PERMISSION = "box.admin.command.gui.other";
+    private static final Component CANNOT_OPEN_MENU = translatable("box.gui.cannot-open-menu", RED);
+    private static final Component COMMAND_HELP =
+            translatable("box.gui.command-help.command-line", AQUA)
+                    .append(text(" - ", DARK_GRAY))
+                    .append(translatable("box.gui.command-help.description", GRAY));
 
     public MenuOpenCommand() {
         super("gui", "box.command.gui", Set.of("g", "menu", "m"));
@@ -68,14 +74,20 @@ public class MenuOpenCommand extends AbstractCommand {
 
         session.setStockHolder(stockHolder);
 
+        var menu = new CategorySelectorMenu();
+        var event = new MenuOpenEvent(player, menu);
+
+        if (BoxProvider.get().getEventBus().callEvent(event).isCancelled()) {
+            sender.sendMessage(CANNOT_OPEN_MENU);
+            return;
+        }
+
         MenuOpener.open(new CategorySelectorMenu(), player);
     }
 
     @Override
     public @NotNull Component getHelp() {
-        return translatable("box.gui.command-help.command-line", AQUA)
-                .append(text(" - ", DARK_GRAY))
-                .append(translatable("box.gui.command-help.description", GRAY));
+        return COMMAND_HELP;
     }
 
     @Override
