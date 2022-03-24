5b0778e7dc9266b37feb3224977b422949a73a57
Siroshun09 <siro.shun8@gmail.com>
2022-03-24 17:31:57 +0900

    build: update event4j to 3.0.0

--- build-logic/src/main/kotlin/box.dependencies.gradle.kts
+++ build-logic/src/main/kotlin/box.dependencies.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation("com.github.siroshun09.configapi:configapi-yaml:4.6.0")
-    implementation("com.github.siroshun09.event4j:event4j:2.2.0")
+    implementation("com.github.siroshun09.event4j:event4j:3.0.0")
 
     compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:23.0.0")
