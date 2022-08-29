f9c71b89138099abb4910ba4e1ce96310bac1574
Siroshun09 <siro.shun8@gmail.com>
2022-08-29 10:09:07 +0900

    build: add box.upload for GitHub Actions

--- build-logic/src/main/kotlin/box.common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/box.common-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     `java-library`
 
@@ -5,12 +7,20 @@ plugins {
     id("box.publication")
 }
 
+val toUpload = findProperty("box.upload")?.toString()?.toBoolean() ?: false
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
+    build {
+        if (toUpload) {
+            buildDir = rootProject.buildDir.resolve(project.name)
+        }
+    }
+
     compileJava {
         options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
@@ -19,4 +29,27 @@ tasks {
     processResources {
         filteringCharset = Charsets.UTF_8.name()
     }
+
+    jar {
+        var version = project.version.toString()
+
+        if (toUpload) { // for GitHub Actions
+            version = "${project.version}-git-${getLatestCommitHash()}"
+        }
+
+        manifest {
+            attributes(
+                "Implementation-Version" to version
+            )
+        }
+    }
+}
+
+fun getLatestCommitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-parse", "--short=7", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
 }
