3736b959f9b754e01b6860ea03802950cd91964e
Siroshun09 <siro.shun8@gmail.com>
2022-09-17 02:39:11 +0900

    build(stick): add lwc api

--- features/stick/build.gradle.kts
+++ features/stick/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     id("box.common-conventions")
 }
 
+repositories {
+    maven {
+        url = uri("https://repo.codemc.io/repository/maven-public/")
+    }
+}
+
 dependencies {
     compileOnly(project(":box-api"))
+    compileOnly(libs.lwc)
 }
