8c9845ffe68ae978add41f1282ddfabb6e690b3b
Siroshun09 <siro.shun8@gmail.com>
2022-10-27 06:24:57 +0900

    style(command): order of imports

--- features/command/src/main/java/net/okocraft/box/feature/command/boxadmin/stock/StockInfoCommand.java
+++ features/command/src/main/java/net/okocraft/box/feature/command/boxadmin/stock/StockInfoCommand.java
@@ -4,9 +4,9 @@ import net.kyori.adventure.text.Component;
 import net.okocraft.box.api.BoxProvider;
 import net.okocraft.box.api.command.AbstractCommand;
 import net.okocraft.box.api.message.GeneralMessage;
-import net.okocraft.box.feature.command.message.BoxAdminMessage;
 import net.okocraft.box.api.util.TabCompleter;
 import net.okocraft.box.api.util.UserStockHolderOperator;
+import net.okocraft.box.feature.command.message.BoxAdminMessage;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
