cef6cb94afa6a89972172e8e983d0930903246f7
Siroshun09 <siro.shun8@gmail.com>
2023-03-31 20:10:27 +0900

    change(api): remove ScheduledForRemoval annotation

--- api/src/main/java/net/okocraft/box/api/taskfactory/TaskFactory.java
+++ api/src/main/java/net/okocraft/box/api/taskfactory/TaskFactory.java
@@ -1,7 +1,6 @@
 package net.okocraft.box.api.taskfactory;
 
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
@@ -21,8 +20,7 @@ public interface TaskFactory {
      * @return the new {@link CompletableFuture}
      * @deprecated not working on Folia
      */
-    @Deprecated(since = "5.3.1", forRemoval = true)
-    @ApiStatus.ScheduledForRemoval(inVersion = "5.4.0")
+    @Deprecated(since = "5.3.1")
     @NotNull CompletableFuture<Void> run(@NotNull Runnable task);
 
     @NotNull CompletableFuture<Void> runTaskForPlayer(@NotNull Player target, @NotNull Consumer<Player> task);
@@ -35,8 +33,7 @@ public interface TaskFactory {
      * @return the new {@link CompletableFuture}
      * @deprecated not working on Folia
      */
-    @Deprecated(since = "5.3.1", forRemoval = true)
-    @ApiStatus.ScheduledForRemoval(inVersion = "5.4.0")
+    @Deprecated(since = "5.3.1")
     <T> @NotNull CompletableFuture<T> supply(@NotNull Supplier<T> supplier);
 
     <T> @NotNull CompletableFuture<T> supplyFromPlayer(@NotNull Player player, @NotNull Function<Player, T> function);
