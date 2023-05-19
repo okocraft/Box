5388465dd74fa2bd96dc1de4b5f6aba3acee035f
Siroshun09 <siro.shun8@gmail.com>
2023-05-20 02:00:56 +0900

    feat(category): add default categories.yml for 1.20 (currently same as 1.19.3)

--- features/category/src/main/java/net/okocraft/box/feature/category/internal/file/BundledCategoryFile.java
+++ features/category/src/main/java/net/okocraft/box/feature/category/internal/file/BundledCategoryFile.java
@@ -24,11 +24,13 @@ public final class BundledCategoryFile {
             version = "1_18";
         } else if (currentVer.isBeforeOrSame(MCDataVersion.MC_1_19_2)) { // 1.19, 1.19.1, 1.19.2
             version = "1_19";
-        } else if (currentVer.isAfterOrSame(MCDataVersion.MC_1_19_3)) { // 1.19.3, 1.19.4
+        } else if (currentVer.isBeforeOrSame(MCDataVersion.MC_1_19_4)) { // 1.19.3, 1.19.4
             version = "1_19_3";
+        } else if (currentVer.isAfterOrSame(MCDataVersion.MC_1_20)) { // 1.20
+            version = "1_20";
         } else {
             // Future version? Use latest categories.yml
-            version = "1_19_3";
+            version = "1_20";
         }
 
         return "categories_" + version + ".yml";
