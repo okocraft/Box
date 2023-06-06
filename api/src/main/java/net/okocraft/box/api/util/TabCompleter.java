1f0e1d6a3c629847399af13424117750f206874c
Siroshun09 <siro.shun8@gmail.com>
2023-06-07 03:06:10 +0900

    improve(api): allow null for the permission node of TabCompleter#players

--- api/src/main/java/net/okocraft/box/api/util/TabCompleter.java
+++ api/src/main/java/net/okocraft/box/api/util/TabCompleter.java
@@ -4,6 +4,7 @@ import net.okocraft.box.api.BoxProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.HumanEntity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Locale;
@@ -21,7 +22,7 @@ public final class TabCompleter {
      * @return the list of item names that match the filter
      */
     public static @NotNull List<String> itemNames(@NotNull String filter) {
-        var itemNameFilter = filter.toUpperCase(Locale.ROOT);
+        var itemNameFilter = filter.toUpperCase(Locale.ENGLISH);
         return BoxProvider.get()
                 .getItemManager()
                 .getItemNameSet()
@@ -37,14 +38,7 @@ public final class TabCompleter {
      * @return the list of player names that match the filter
      */
     public static @NotNull List<String> players(@NotNull String filter) {
-        var playerNameFilter = filter.toLowerCase(Locale.ROOT);
-
-        return Bukkit.getOnlinePlayers()
-                .stream()
-                .map(HumanEntity::getName)
-                .map(name -> name.toLowerCase(Locale.ROOT))
-                .filter(playerName -> playerName.startsWith(playerNameFilter))
-                .collect(Collectors.toList());
+        return players(filter, null);
     }
 
     /**
@@ -54,15 +48,14 @@ public final class TabCompleter {
      * @param permissionNode the permission node to check
      * @return the list of player names that match the filter
      */
-    public static @NotNull List<String> players(@NotNull String filter, @NotNull String permissionNode) {
-        var playerNameFilter = filter.toLowerCase(Locale.ROOT);
+    public static @NotNull List<String> players(@NotNull String filter, @Nullable String permissionNode) {
+        var playerNameFilter = filter.toLowerCase(Locale.ENGLISH);
 
         return Bukkit.getOnlinePlayers()
                 .stream()
-                .filter(player -> player.hasPermission(permissionNode))
+                .filter(player -> permissionNode == null || player.hasPermission(permissionNode))
                 .map(HumanEntity::getName)
-                .map(name -> name.toLowerCase(Locale.ROOT))
-                .filter(playerName -> playerName.startsWith(playerNameFilter))
+                .filter(playerName -> playerName.toLowerCase(Locale.ENGLISH).startsWith(playerNameFilter))
                 .collect(Collectors.toList());
     }
 }
