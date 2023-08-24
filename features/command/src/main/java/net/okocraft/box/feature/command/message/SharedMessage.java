c0628b70af091fe7f7c960bcd1db8059acda24a5
Siroshun09 <siro.shun8@gmail.com>
2023-08-25 04:21:28 +0900

    clean(command): replace deprecated methods

--- features/command/src/main/java/net/okocraft/box/feature/command/message/SharedMessage.java
+++ features/command/src/main/java/net/okocraft/box/feature/command/message/SharedMessage.java
@@ -1,9 +1,10 @@
 package net.okocraft.box.feature.command.message;
 
+import it.unimi.dsi.fastutil.objects.ObjectIntPair;
 import net.kyori.adventure.text.Component;
 import net.okocraft.box.api.message.argument.DoubleArgument;
 import net.okocraft.box.api.message.argument.TripleArgument;
-import net.okocraft.box.api.model.stock.StockData;
+import net.okocraft.box.api.model.item.BoxItem;
 
 import static net.okocraft.box.api.message.Components.aquaItemName;
 import static net.okocraft.box.api.message.Components.aquaText;
@@ -20,10 +21,10 @@ public final class SharedMessage {
                     aquaText(target), aquaText(page), aquaText(maxPage)
             );
 
-    public static final DoubleArgument<Integer, StockData> STOCK_LIST_ITEM_AMOUNT =
-            (num, stockData) -> grayTranslatable(
+    public static final DoubleArgument<Integer, ObjectIntPair<BoxItem>> STOCK_LIST_ITEM_AMOUNT =
+            (num, itemAmountPair) -> grayTranslatable(
                     "box.command.shared.stock-list.amount",
-                    grayText(num), aquaItemName(stockData.item()), aquaText(stockData.amount())
+                    grayText(num), aquaItemName(itemAmountPair.left()), aquaText(itemAmountPair.rightInt())
             );
 
     public static final Component NO_STOCK_FOUND = redTranslatable("box.command.shared.stock-list.no-stock-found");
