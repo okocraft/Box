a48abb0fb2b7630596db319061d7b8b8ec05a28a
Siroshun09 <siro.shun8@gmail.com>
2023-12-22 03:24:11 +0900

    feat(api): add BoxAPI#api and BoxAPI#isLoaded

--- api/src/main/java/net/okocraft/box/api/BoxProvider.java
+++ api/src/main/java/net/okocraft/box/api/BoxProvider.java
@@ -1,5 +1,6 @@
 package net.okocraft.box.api;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -7,15 +8,17 @@ import java.util.Objects;
 /**
  * A class to provide {@link BoxAPI}.
  */
+@ApiStatus.Obsolete
 public final class BoxProvider {
 
-    private static BoxAPI API;
+    static BoxAPI API;
 
     /**
      * Gets a {@link BoxAPI}.
      *
      * @return a {@link BoxAPI}
      * @throws IllegalStateException if {@link BoxAPI} is not loaded
+     * @see BoxAPI#api()
      */
     public static @NotNull BoxAPI get() throws IllegalStateException {
         if (API != null) {
@@ -30,7 +33,11 @@ public final class BoxProvider {
      *
      * @param api a {@link BoxAPI}
      * @throws IllegalStateException if {@link BoxAPI} is already set
+     * @deprecated this method is for internal, and will be removed in v6
      */
+    @Deprecated(since = "5.5.2", forRemoval = true)
+    @ApiStatus.ScheduledForRemoval(inVersion = "6.0.0")
+    @ApiStatus.Internal
     public static void set(@NotNull BoxAPI api) {
         if (API == null) {
             API = Objects.requireNonNull(api);
