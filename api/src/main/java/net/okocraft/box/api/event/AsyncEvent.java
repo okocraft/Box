0f7bedbe5a395459fb555af13bc09026915494c1
Siroshun09 <siro.shun8@gmail.com>
2023-12-22 03:28:17 +0900

    docs(api): make AsyncEvent deprecated for removal

--- api/src/main/java/net/okocraft/box/api/event/AsyncEvent.java
+++ api/src/main/java/net/okocraft/box/api/event/AsyncEvent.java
@@ -1,7 +1,12 @@
 package net.okocraft.box.api.event;
 
+import org.jetbrains.annotations.ApiStatus;
+
 /**
  * An interface representing an event that is called in a different thread than the thread that processes the Box.
+ * @deprecated will be removed in v6
  */
+@Deprecated(since = "5.5.2", forRemoval = true)
+@ApiStatus.ScheduledForRemoval(inVersion = "6.0.0")
 public interface AsyncEvent {
 }
