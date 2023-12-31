21cb58637f775b792189613592e328c99921d710
Siroshun09 <siro.shun8@gmail.com>
2023-12-31 09:14:41 +0900

    feat(api): add ItemVersion#of

--- api/src/main/java/net/okocraft/box/api/model/item/ItemVersion.java
+++ api/src/main/java/net/okocraft/box/api/model/item/ItemVersion.java
@@ -4,6 +4,8 @@ import net.okocraft.box.api.util.MCDataVersion;
 import net.okocraft.box.api.util.Version;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 /**
  * A record that represents the item version of {@link BoxItem}s.
  *
@@ -12,6 +14,38 @@ import org.jetbrains.annotations.NotNull;
  */
 public record ItemVersion(@NotNull MCDataVersion dataVersion,
                           int defaultItemVersion) implements Version<ItemVersion> {
+
+    /**
+     * Creates an {@link ItemVersion} with the specified data version.
+     *
+     * @param dataVersion the {@link MCDataVersion}
+     * @return a new {@link ItemVersion} with the specified data version
+     */
+    public static @NotNull ItemVersion of(int dataVersion) {
+        return of(dataVersion, 0);
+    }
+
+    /**
+     * Creates an {@link ItemVersion} with the specified data version and default item version.
+     *
+     * @param dataVersion        the {@link MCDataVersion}
+     * @param defaultItemVersion the version of the provider that generates {@link BoxDefaultItem}s
+     * @return a new {@link ItemVersion} with the specified data version default item version
+     */
+    public static @NotNull ItemVersion of(int dataVersion, int defaultItemVersion) {
+        return new ItemVersion(MCDataVersion.of(dataVersion), defaultItemVersion);
+    }
+
+    /**
+     * The constructor of {@link ItemVersion}.
+     *
+     * @param dataVersion        the {@link MCDataVersion}
+     * @param defaultItemVersion the version of the provider that generates {@link BoxDefaultItem}s
+     */
+    public ItemVersion {
+        Objects.requireNonNull(dataVersion);
+    }
+
     @Override
     public int compareTo(@NotNull ItemVersion other) {
         int i = this.dataVersion.compareTo(other.dataVersion);
