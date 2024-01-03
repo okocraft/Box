f59b7c63745ba3d8e2ea051b404186a8eafe59b9
Siroshun09 <siro.shun8@gmail.com>
2024-01-04 00:51:55 +0900

    docs(api): make AutoSaveStartEvent deprecated for removal

--- api/src/main/java/net/okocraft/box/api/event/general/AutoSaveStartEvent.java
+++ api/src/main/java/net/okocraft/box/api/event/general/AutoSaveStartEvent.java
@@ -1,11 +1,16 @@
 package net.okocraft.box.api.event.general;
 
 import net.okocraft.box.api.event.BoxEvent;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * An event called when the auto save task started.
+ *
+ * @deprecated This event will be no longer called in Box v6
  */
+@Deprecated(forRemoval = true, since = "5.5.2")
+@ApiStatus.ScheduledForRemoval(inVersion = "6.0.0")
 public class AutoSaveStartEvent extends BoxEvent {
 
     @Override
--- api/src/main/java/net/okocraft/box/api/event/general/package-info.java
+++ api/src/main/java/net/okocraft/box/api/event/general/package-info.java
@@ -1,4 +1,10 @@
 /**
  * A package of general events.
+ *
+ * @deprecated This package will be removed in Box v6
  */
+@Deprecated(forRemoval = true, since = "5.5.2")
+@ApiStatus.ScheduledForRemoval(inVersion = "6.0.0")
 package net.okocraft.box.api.event.general;
+
+import org.jetbrains.annotations.ApiStatus;
