3131b6d1ffa2562bab08f76e3100aaa40af633a5
Siroshun09 <siro.shun8@gmail.com>
2024-02-09 03:13:28 +0900

    feat(test-shared-classes): add a mode that throws exception on method calling to MemoryUserStorage

--- test-shared-classes/src/main/java/net/okocraft/box/test/shared/storage/memory/user/MemoryUserStorage.java
+++ test-shared-classes/src/main/java/net/okocraft/box/test/shared/storage/memory/user/MemoryUserStorage.java
@@ -16,6 +16,8 @@ public class MemoryUserStorage implements UserStorage {
 
     private final Map<UUID, String> uuidToNameMap = new HashMap<>();
     private final Map<String, UUID> nameToUuidMap = new HashMap<>();
+    private boolean throwExceptionMode = false;
+    private RuntimeException thrownException = null;
 
     @Override
     public void init() {
@@ -23,11 +25,14 @@ public class MemoryUserStorage implements UserStorage {
 
     @Override
     public @NotNull BoxUser loadBoxUser(@NotNull UUID uuid) {
+        this.throwExceptionIfEnabled();
         return BoxUserFactory.create(uuid, this.uuidToNameMap.get(uuid));
     }
 
     @Override
     public void saveBoxUser(@NotNull UUID uuid, @Nullable String name) {
+        this.throwExceptionIfEnabled();
+
         var oldName = this.uuidToNameMap.put(uuid, name);
 
         if (oldName != null) {
@@ -41,12 +46,16 @@ public class MemoryUserStorage implements UserStorage {
 
     @Override
     public @Nullable BoxUser searchByName(@NotNull String name) {
+        this.throwExceptionIfEnabled();
+
         var uuid = this.nameToUuidMap.get(name);
         return uuid != null ? BoxUserFactory.create(uuid, name) : null;
     }
 
     @Override
     public @NotNull Collection<BoxUser> loadAllBoxUsers() {
+        this.throwExceptionIfEnabled();
+
         var list = new ArrayList<BoxUser>(this.uuidToNameMap.size());
 
         for (var entry : this.uuidToNameMap.entrySet()) {
@@ -58,8 +67,25 @@ public class MemoryUserStorage implements UserStorage {
 
     @Override
     public void saveBoxUsers(@NotNull Collection<BoxUser> users) {
+        this.throwExceptionIfEnabled();
+
         for (var user : users) {
             saveBoxUser(user.getUUID(), user.getName().orElse(null));
+        }
+    }
+
+    public @Nullable RuntimeException getThrownException() {
+        return this.thrownException;
+    }
+
+    public void setThrowExceptionMode(boolean newState) {
+        this.throwExceptionMode = newState;
+    }
+
+    private void throwExceptionIfEnabled() {
+        if (this.throwExceptionMode) {
+            this.thrownException = new RuntimeException();
+            throw this.thrownException;
         }
     }
 }
