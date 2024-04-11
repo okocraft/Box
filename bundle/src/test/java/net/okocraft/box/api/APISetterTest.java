b122524f06e410eeb2ac4061fce09db30704e637
Siroshun09 <siro.shun8@gmail.com>
2024-04-11 17:00:43 +0900

    test(api): add tests of APISetter

---
+++ bundle/src/test/java/net/okocraft/box/api/APISetterTest.java
@@ -0,0 +1,40 @@
+package net.okocraft.box.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class APISetterTest {
+
+    @Test
+    void testSetAndUnset() {
+        var api = Mockito.mock(BoxAPI.class);
+
+        APISetter.set(api);
+
+        Assertions.assertSame(api, BoxAPI.api());
+        Assertions.assertSame(api, BoxProvider.API);
+        Assertions.assertSame(api, BoxProvider.get());
+
+        APISetter.unset();
+
+        Assertions.assertThrows(IllegalStateException.class, BoxAPI::api);
+        Assertions.assertNull(BoxProvider.API);
+        Assertions.assertThrows(IllegalStateException.class, BoxProvider::get);
+    }
+
+    @Test
+    void testIllegalState() {
+        Assertions.assertThrows(IllegalStateException.class, APISetter::unset); // unset when no API is set
+
+        var api = Mockito.mock(BoxAPI.class);
+
+        APISetter.set(api);
+
+        Assertions.assertThrows(IllegalStateException.class, () -> APISetter.set(api)); // duplicated API setting
+
+        APISetter.unset();
+
+        Assertions.assertThrows(IllegalStateException.class, APISetter::unset); // unset when no API is set
+    }
+}
