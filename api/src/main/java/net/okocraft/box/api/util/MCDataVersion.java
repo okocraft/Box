6472e8fbfa6946bf06be60d5fc68cf8e92e66f22
Siroshun09 <siro.shun8@gmail.com>
2024-06-14 00:44:46 +0900

    feat(api): add MCDataVersion#MC_1_21

--- api/src/main/java/net/okocraft/box/api/util/MCDataVersion.java
+++ api/src/main/java/net/okocraft/box/api/util/MCDataVersion.java
@@ -98,6 +98,11 @@ public record MCDataVersion(int dataVersion) implements Version<MCDataVersion> {
     public static final MCDataVersion MC_1_20_6 = new MCDataVersion(3839);
 
     /**
+     * A {@link MCDataVersion} that represents Minecraft 1.21
+     */
+    public static final MCDataVersion MC_1_21 = new MCDataVersion(3953);
+
+    /**
      * Creates a {@link MCDataVersion} from the specified data version
      *
      * @param dataVersion the data version
