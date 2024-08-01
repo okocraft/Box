6d01c35a2aec4950637bba711af8dcfe8029c0a5
Siroshun09 <siro.shun8@gmail.com>
2024-08-01 22:16:49 +0900

    fix(bundle,stick): fix WorldGuardIntegration

--- features/stick/src/main/java/net/okocraft/box/feature/stick/function/container/ChestAccessChecker.java
+++ features/stick/src/main/java/net/okocraft/box/feature/stick/function/container/ChestAccessChecker.java
@@ -2,6 +2,7 @@ package net.okocraft.box.feature.stick.function.container;
 
 import net.okocraft.box.feature.stick.integration.ProtectionPluginIntegration;
 import net.okocraft.box.feature.stick.integration.WorldGuardIntegration;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Container;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +10,12 @@ import org.jetbrains.annotations.NotNull;
 public final class ChestAccessChecker {
 
     public static boolean canAccess(@NotNull Player player, @NotNull Container container, @NotNull ContainerOperation.OperationType operationType) {
-        return ProtectionPluginIntegration.canModify(player, container, operationType) && WorldGuardIntegration.canModify(player, container.getLocation());
+        if (!ProtectionPluginIntegration.canModify(player, container, operationType)) {
+            return false;
+        } else if (Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) {
+            return WorldGuardIntegration.canModify(player, container.getLocation());
+        } else {
+            return true;
+        }
     }
 }
--- features/stick/src/main/java/net/okocraft/box/feature/stick/integration/WorldGuardIntegration.java
+++ features/stick/src/main/java/net/okocraft/box/feature/stick/integration/WorldGuardIntegration.java
@@ -4,7 +4,6 @@ import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.flags.Flags;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -12,13 +11,9 @@ import org.jetbrains.annotations.NotNull;
 public class WorldGuardIntegration {
 
     public static boolean canModify(@NotNull Player player, @NotNull Location location) {
-        if (Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) {
-            return WorldGuard.getInstance().getPlatform().getRegionContainer()
-                    .createQuery().getApplicableRegions(BukkitAdapter.adapt(location))
-                    .testState(WorldGuardPlugin.inst().wrapPlayer(player), Flags.CHEST_ACCESS);
-        } else {
-            return false;
-        }
+        return WorldGuard.getInstance().getPlatform().getRegionContainer()
+                .createQuery()
+                .testBuild(BukkitAdapter.adapt(location), WorldGuardPlugin.inst().wrapPlayer(player), Flags.CHEST_ACCESS);
     }
 
     private WorldGuardIntegration() {
